0703b2d3cbff43852a6f79310a42ac0689d39642
Martin Dakov <martindakov6@gmail.com>
2023-03-05 10:29:29 +0200

    Add new files
    For the conditional statements

---
+++ C# Basics/Conditional Statements/01.Excellent Result/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ConditionalStatementsLab
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double currentScore = double.Parse(Console.ReadLine());
+            bool isExelletnScore = currentScore >= 5.5;
+            if (isExelletnScore) 
+            {
+                Console.WriteLine("Excellent!");
+            }
+        }
+       
+    }
+}
