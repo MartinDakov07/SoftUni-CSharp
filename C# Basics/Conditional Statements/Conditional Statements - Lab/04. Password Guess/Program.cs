80a414170f4ece9bf90dba54f13a59d8bc7b4485
Martin Dakov <martindakov6@gmail.com>
2023-03-05 22:55:30 +0200

    Add more files

---
+++ C# Basics/Conditional Statements/Conditional Statements - Lab/04. Password Guess/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _04._Password_Guess
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string passwordFromTheUser = Console.ReadLine();
+            string password = "s3cr3t!P@ssw0rd";
+            if (passwordFromTheUser == password) 
+            {
+                Console.WriteLine("Welcome");
+            }
+            else
+            {
+                Console.WriteLine("Wrong password!");
+            }
+        }
+    }
+}
