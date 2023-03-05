264b2020b99954eea369280a8991e03ea136b4e0
Martin Dakov <martindakov6@gmail.com>
2023-03-05 22:54:52 +0200

    Add two new files

---
+++ C# Basics/Conditional Statements/Conditional Statements - Lab/03. Even or Odd/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _03._Even_or_Odd
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+            bool evenOrOddNum = number % 2 == 0;
+            if (evenOrOddNum)
+            {
+                Console.WriteLine("even");
+            }
+            else
+            {
+                Console.WriteLine("odd");
+            }
+        }
+    }
+}
