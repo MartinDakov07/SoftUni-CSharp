a09b47ce8749b4b4af795dfee1ebee7055be0354
Martin Dakov <martindakov6@gmail.com>
2023-03-06 21:51:30 +0200

    Add the last file

---
+++ C# Basics/Conditional Statements/Conditional Statements - Exercise/08. Lunch Break/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace _08._Lunch_Break
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string film = Console.ReadLine();
+            int filmTime = int.Parse(Console.ReadLine());
+            int seriesBreak = int.Parse(Console.ReadLine());
+            double timeForSeries = seriesBreak * 5 / 8.0;
+            double difference = timeForSeries - filmTime;
+            if (timeForSeries >= filmTime)
+            {
+                Console.WriteLine($"You have enough time to watch {film} and left with {Math.Ceiling(difference)} minutes free time.");
+            }
+            else
+            {
+                Console.WriteLine($"You don't have enough time to watch {film}, you need {Math.Ceiling(filmTime - timeForSeries)} more minutes.");
+            }
+        }
+    }
+}
