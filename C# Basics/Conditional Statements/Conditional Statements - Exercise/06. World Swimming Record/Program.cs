a24bfc7f8f1c31cb72521920b24eb7322f40c33b
Martin Dakov <martindakov6@gmail.com>
2023-03-06 21:51:05 +0200

    Add two more files

---
+++ C# Basics/Conditional Statements/Conditional Statements - Exercise/06. World Swimming Record/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace _06._World_Swimming_Record
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double record = double.Parse(Console.ReadLine());
+            double meters = double.Parse(Console.ReadLine());
+            double secFor1m = double.Parse(Console.ReadLine());
+            //Izchislqvame kolko sec shte izmine za x metra
+            //съпротивлението на водата го забавя на всеки 15 м. с 12.5 sekundi
+            double metersDownPerMeters = Math.Floor(meters / 15);
+            double metersDown = metersDownPerMeters * 12.5;
+            double allTime = meters * secFor1m;
+            //Obshto vreme na Ivan 
+            double totalTime = allTime + metersDown; // vreme na Ivan = totalTime
+
+            //Razlika v rekorda na Ivan i 
+             
+            if (totalTime >= record )
+            {
+                double difference = record - totalTime;
+                Console.WriteLine($"No, he failed! He was {Math.Abs(difference):f2} seconds slower.");
+                
+            }
+            else
+            {
+                Console.WriteLine($"Yes, he succeeded! The new world record is {(totalTime):f2} seconds.");
+            }
+        }
+    }
+}
---
+++ C# Basics/Conditional Statements/Conditional Statements - Exercise/07. Shopping/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace _07._Shopping
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //zapisvame si ot konzolata broqt na Budgeta na Petyr i broikite na produktite 
+            double budget = double.Parse(Console.ReadLine());
+            int gpu = int.Parse(Console.ReadLine());
+            int cpu = int.Parse(Console.ReadLine());
+            int ram = int.Parse(Console.ReadLine());
+            //namirane na cena za video kartata
+            int gpuPrice = gpu * 250;
+            //cena za procesora
+            double sumForCpus = cpu * gpuPrice * 0.35;
+            // cena za Ram pametta
+            double sumForRam = ram * gpuPrice * 0.10;
+            double totalPrice = gpuPrice + sumForCpus + sumForRam;  ;
+            if (gpu > cpu) // budget - totalPrice
+            {
+                totalPrice *= 0.85;
+            }
+            if (budget >= totalPrice)
+            {
+               
+                Console.WriteLine($"You have {budget - totalPrice:f2} leva left!");
+            }
+            else
+            {
+                Console.WriteLine($"Not enough money! You need {totalPrice - budget:f2} leva more!");
+            }
+        }
+    }
+}
