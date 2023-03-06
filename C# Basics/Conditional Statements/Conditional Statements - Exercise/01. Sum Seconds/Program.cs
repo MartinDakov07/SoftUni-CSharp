a61f64f27399a388aca066dabd96ebff99657b56
Martin Dakov <martindakov6@gmail.com>
2023-03-06 21:48:13 +0200

    Add first file

---
+++ C# Basics/Conditional Statements/Conditional Statements - Exercise/01. Sum Seconds/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Conditional_Statements___Exercise
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstRunner = int.Parse(Console.ReadLine());
+            int secondRunner = int.Parse(Console.ReadLine());
+            int thirdRunner = int.Parse(Console.ReadLine());
+            int timeForAllRunners = firstRunner + secondRunner + thirdRunner;
+            int min = timeForAllRunners / 60;
+            int sec = timeForAllRunners % 60;
+            if (sec <10)
+            {
+                Console.WriteLine($"{min}:0{sec}");
+            }
+            else
+            {
+                Console.WriteLine($"{min}:{sec}");
+            }
+        }
+    }
+}
