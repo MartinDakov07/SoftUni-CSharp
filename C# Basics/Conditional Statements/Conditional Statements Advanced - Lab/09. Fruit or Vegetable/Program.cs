6627d2a7e4ed164e3da827f51688ee8d7ba2f323
Martin Dakov <martindakov6@gmail.com>
2023-03-07 22:11:40 +0200

    Add 9th file

---
+++ C# Basics/Conditional Statements/Conditional Statements Advanced - Lab/09. Fruit or Vegetable/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace _09._Fruit_or_Vegetable
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Плодовете "fruit" имат следните възможни стойности: banana, apple, kiwi, cherry, lemon и grapes
+
+            //  Зеленчуците "vegetable" имат следните възможни стойности: tomato, cucumber, pepper и carrot
+
+            //  Всички останали са "unknown"
+            string product = Console.ReadLine();
+            switch (product)
+            {
+                case "banana":
+                    
+                case "apple":
+                    
+                case "kiwi":
+                    
+                case "cherry":
+                   
+                case "lemon":
+ 
+                case "grapes":
+                    Console.WriteLine("fruit");
+                    break;
+                case "pepper":
+
+                case "tomato":
+
+                case "cucumber":
+
+                case "carrot":
+                    Console.WriteLine("vegetable");
+                    break;
+
+                default:
+                    Console.WriteLine("unknown");
+                    break;
+            }
+        }
+    }
+}
