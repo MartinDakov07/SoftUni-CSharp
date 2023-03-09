5537800c27db9de0660f62246c3cbad3c3c8c869
Martin Dakov <martindakov6@gmail.com>
2023-03-09 22:22:30 +0200

    add 4th file

---
+++ C# Basics/For Loop/For Loop - Exercise/04. Clever Lily/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace _04._Clever_Lily
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int age = int.Parse(Console.ReadLine());
+            double laundry = double.Parse(Console.ReadLine());
+            int toyPrice = int.Parse(Console.ReadLine());
+            double money = 0;
+            for(int i =1; i <=age; i++)
+            {
+                if (i%2==0)
+                {
+                    money += i * 5 - 1;
+                }
+                else
+                {
+                    money += toyPrice;
+                }               
+            }
+            if (money >= laundry)
+            {
+                Console.WriteLine($"Yes! {(money - laundry):f2}");
+            }
+            else
+            {
+                Console.WriteLine($"No! {(laundry - money):f2}");
+            }
+        }
+    }
+}
