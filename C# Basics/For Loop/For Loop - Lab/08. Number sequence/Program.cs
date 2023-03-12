59a0de30c7944fe006b9c4c4d0b6333794cc62c9
Martin Dakov <martindakov6@gmail.com>
2023-03-12 21:06:20 +0200

    add more files

---
+++ C# Basics/For Loop/For Loop - Lab/08. Number sequence/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace _08._Number_sequence
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int countOfInputs = int.Parse(Console.ReadLine());
+            int minNumber = int.MaxValue;
+            int maxNumber = int.MinValue;
+            for(int counter = 0; counter < countOfInputs;counter++)
+            {
+                int currentNumber = int.Parse(Console.ReadLine());
+                if(currentNumber > maxNumber)
+                {
+                    maxNumber = currentNumber;  
+                }
+                
+
+                if (currentNumber < minNumber)
+                {
+                    minNumber = currentNumber;
+                }
+            }
+            Console.WriteLine($"Max number: {maxNumber}");
+            Console.WriteLine($"Min number: {minNumber}");
+        }
+    }
+}
