f4130c21b17461ce726223de8ade5feb346d2f6c
Martin Dakov <martindakov6@gmail.com>
2023-03-12 21:03:32 +0200

    Add file

---
+++ C# Basics/For Loop/For Loop - Lab/01. Numbers from 1 to 100/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace For_Loop___Lab
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int n = 1; n <= 100; n++)
+            {
+                Console.WriteLine(n);
+            }
+        }
+    }
+}
