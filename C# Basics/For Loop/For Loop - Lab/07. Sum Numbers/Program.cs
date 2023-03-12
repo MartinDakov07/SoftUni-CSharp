d74aa8d62ba827b2b019c4523b499fa89931d910
Martin Dakov <martindakov6@gmail.com>
2023-03-12 21:05:50 +0200

    add 7th file

---
+++ C# Basics/For Loop/For Loop - Lab/07. Sum Numbers/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace _07._Sum_Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int countOfNums = int.Parse(Console.ReadLine());
+            int sumOfNumbers = 0;
+            for(int counter = 0; counter <countOfNums; counter++)
+            {
+                int currentNumber = int.Parse(Console.ReadLine());
+                sumOfNumbers += currentNumber;
+            }
+            Console.WriteLine(sumOfNumbers);
+        }
+    }
+}
