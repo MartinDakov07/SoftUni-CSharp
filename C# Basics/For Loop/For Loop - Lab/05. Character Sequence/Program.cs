3fab217a3a73c024fae950ce2b2dfaf174024164
Martin Dakov <martindakov6@gmail.com>
2023-03-12 21:05:13 +0200

    Add 5th file

---
+++ C# Basics/For Loop/For Loop - Lab/05. Character Sequence/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace _05._Character_Sequence
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string word = Console.ReadLine();
+            int length = word.Length;
+            for (int index = 0;index <= length - 1;index++)
+            {
+                char currentChar = word[index];
+                Console.WriteLine(currentChar);
+            }
+        }
+    }
+}
