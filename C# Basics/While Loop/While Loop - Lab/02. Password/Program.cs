968a400ec0262f5b4a1081ad5cb5ab9cde793332
Martin Dakov <martindakov6@gmail.com>
2023-03-13 23:00:19 +0200

    adding first two files

---
+++ C# Basics/While Loop/While Loop - Lab/02. Password/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace _02._Password
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string username = Console.ReadLine();
+            string currentPassword = Console.ReadLine();
+            string inputPassword = Console.ReadLine();
+            while(inputPassword != currentPassword)
+            {
+                inputPassword = Console.ReadLine();
+            }
+            Console.WriteLine($"Welcome {username}!");
+        }
+    }
+}
