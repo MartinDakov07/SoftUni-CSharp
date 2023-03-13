7260382eecd9b0bf52d8ed15a241db1530158ece
Martin Dakov <martindakov6@gmail.com>
2023-03-13 23:01:06 +0200

    adding two more

---
+++ C# Basics/While Loop/While Loop - Lab/03. Sum Numbers/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _03._Sum_Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int sumOfAllEntNumbers = 0;
+            while(n > sumOfAllEntNumbers)
+            {
+                int inputNumber = int.Parse(Console.ReadLine());
+                sumOfAllEntNumbers += inputNumber;
+                
+            }
+            
+                Console.WriteLine(sumOfAllEntNumbers);
+            
+        }
+    }
+}
