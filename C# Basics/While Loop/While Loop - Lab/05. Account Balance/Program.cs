74c0bb89b48c52146e992a4d25f8db12bcf952be
Martin Dakov <martindakov6@gmail.com>
2023-03-13 23:01:35 +0200

    adding more more two

---
+++ C# Basics/While Loop/While Loop - Lab/05. Account Balance/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace _05._Account_Balance
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string inputWord = Console.ReadLine();
+            double totalAmount = 0;
+            while (inputWord != "NoMoreMoney")
+            {
+                double currentAmount = double.Parse(inputWord);
+                if (currentAmount < 0)
+                {
+                    Console.WriteLine("Invalid operation!");
+                    break;
+                }
+                Console.WriteLine($"Increase: {currentAmount:f2}");
+                totalAmount += currentAmount;
+                inputWord = Console.ReadLine();
+                
+            }
+            if (totalAmount >= 0)
+            {
+                Console.WriteLine($"Total: {totalAmount:f2}");
+            }
+           
+        }
+    }
+}
