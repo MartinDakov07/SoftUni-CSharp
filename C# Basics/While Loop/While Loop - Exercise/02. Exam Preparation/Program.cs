78dd32a1be8231af6bfa89a0b26022deddd21ae2
Martin Dakov <martindakov6@gmail.com>
2023-03-14 22:41:02 +0200

    add second file

---
+++ C# Basics/While Loop/While Loop - Exercise/02. Exam Preparation/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace _02._Exam_Preparation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int badGrades = int.Parse(Console.ReadLine());
+            string gradeName = Console.ReadLine();
+            int grade;
+            int badCounts = 0;
+            double gradeSum = 0;
+            int solvedProblems = 0;
+            string lastProblem = string.Empty;
+            while(gradeName != "Enough")
+            {
+                grade = int.Parse(Console.ReadLine());
+                
+                gradeSum += grade;
+                solvedProblems++;
+                lastProblem = gradeName;
+                if (grade <= 4)
+                {
+                    badCounts++;
+                    if (badCounts == badGrades)
+                    {
+                        Console.WriteLine($"You need a break, {badCounts} poor grades.");
+                        break;
+                    }
+                }                            
+                
+                gradeName = Console.ReadLine();
+                
+                
+            }
+            if (badCounts < badGrades)
+            {
+                Console.WriteLine($"Average score: {(gradeSum / solvedProblems):f2}");
+                Console.WriteLine($"Number of problems: {solvedProblems}");
+                Console.WriteLine($"Last problem: {lastProblem}");
+                
+            }
+
+
+
+
+        }
+    }
+}
