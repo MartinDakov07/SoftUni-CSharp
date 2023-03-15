14cc8b1897aaa2c1c349f8bd0f8b2f932503fd7d
Martin Dakov <martindakov6@gmail.com>
2023-03-15 23:05:12 +0200

    add file

---
+++ C# Basics/Nested Loops/Nested Loops - Lab/03. Combinations/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace _03._Combinations
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberFromUser = int.Parse(Console.ReadLine());
+            int combination = 0;
+            for (int firstNumber = 0; firstNumber <= numberFromUser; firstNumber++)
+            {
+                for (int secondNumber = 0; secondNumber <= numberFromUser; secondNumber++)
+                {
+                    for (int thirdNumber = 0; thirdNumber <= numberFromUser; thirdNumber++)
+                    {
+                        if (firstNumber + secondNumber + thirdNumber == numberFromUser)
+                        {
+                            combination++;
+                        }                     
+                    }
+                }
+            }
+            Console.WriteLine(combination);
+        }
+    }
+}
