5a89325bae38318783219dee0af1ab65cd8eb43d
Martin Dakov <martindakov6@gmail.com>
2023-03-15 23:06:07 +0200

    add file

---
+++ C# Basics/Nested Loops/Nested Loops - Lab/06. Building/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace _06._Building
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int floor = int.Parse(Console.ReadLine());
+            int room = int.Parse(Console.ReadLine());
+            for (int currFloors = floor; currFloors >= 1; currFloors--)
+            {
+                for(int curRoom = 0; curRoom < room; curRoom++)
+                {
+                    if(currFloors == floor)
+                    {
+                        Console.Write($"L{currFloors}{curRoom} ");
+                    }
+                    else if(currFloors % 2 == 0)
+                    {
+                        Console.Write($"O{currFloors}{curRoom} ");
+                    }
+                    else 
+                    {
+                        Console.Write($"A{currFloors}{curRoom} ");
+                    }
+                }
+                Console.WriteLine();
+                
+            }
+        }
+    }
+}
