23e29afbddec0518e1473867407ef90554f3f780
Martin Dakov <martindakov6@gmail.com>
2023-03-16 22:34:34 +0200

    add file

---
+++ C# Basics/Nested Loops/Nested Loops - Exercise/01. Number Pyramid/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace _01._Number_Pyramid
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int currentNum = 1;
+            bool isBigger = false;
+            for(int rows = 1; rows <= n; rows++)
+            {
+                for (int cols = 1; cols <= rows; cols++)
+                {
+                    if (currentNum > n)
+                    {
+                        isBigger = true;
+                        break;
+                    }
+                    Console.Write(currentNum + " ");
+                    currentNum++;
+                }
+                if (isBigger)
+                {
+                    break;
+                }
+                Console.WriteLine();
+                
+            }
+        }
+    }
+}
