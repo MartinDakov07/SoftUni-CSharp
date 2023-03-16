b9c2c224b074040c815b79ac5501e98527ab5c69
Martin Dakov <martindakov6@gmail.com>
2023-03-16 22:35:37 +0200

    one more exercise

---
+++ C# Basics/Nested Loops/Nested Loops - Exercise/05. Special Numbers/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace _05._Special_Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            for(int i = 1; i < 10; i++)
+            {
+                for (int j = 1; j < 10; j++)
+                {
+                    for (int k = 1; k < 10; k++)
+                    {
+                        for (int l = 1; l < 10; l++)
+                        {
+                            if (n % i == 0 && n % j == 0 && n % k == 0 && n % l == 0)
+                            {
+                                Console.Write($"{i}{j}{k}{l} ");
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
