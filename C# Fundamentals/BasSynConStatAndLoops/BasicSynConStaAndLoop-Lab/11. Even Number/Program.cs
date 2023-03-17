c0e01a08e4effeed0ab8fee221dc520e8a413dda
Martin Dakov <martindakov6@gmail.com>
2023-03-17 22:49:21 +0200

    add last file
    Basic Syntax, Conditional Statements and Loops are ez

---
+++ C# Fundamentals/BasSynConStatAndLoops/BasicSynConStaAndLoop-Lab/11. Even Number/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace _11._Even_Number
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+            while (true)
+            {
+                if(num % 2 == 0)
+                {
+                    Console.WriteLine($"The number is: {Math.Abs(num)}");
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Please write an even number.");
+                }
+                num = int.Parse(Console.ReadLine());
+            }
+        }
+    }
+}
