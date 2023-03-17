e77bd056b731666a3f3282ecfbaaabfc1321969f
Martin Dakov <martindakov6@gmail.com>
2023-03-17 22:48:21 +0200

    add two more files

---
+++ C# Fundamentals/BasSynConStatAndLoops/BasicSynConStaAndLoop-Lab/09. Multiplication Table/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace _09._Multiplication_Table
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int multiplier = int.Parse(Console.ReadLine());
+            
+            for(int i = 1; i <= 10; i++)
+            {
+                Console.WriteLine($"{multiplier} X {i} = {multiplier * i}");
+            }
+        }
+    }
+}
---
+++ C# Fundamentals/BasSynConStatAndLoops/BasicSynConStaAndLoop-Lab/10. Multiplication Table 2.0/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace _10._Multiplication_Table_2._0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int integer = int.Parse(Console.ReadLine());
+            int multiplier = int.Parse(Console.ReadLine());
+            if (multiplier <= 10)
+            {
+                for (int i = multiplier; i <= 10; i++)
+                {
+
+                    Console.WriteLine($"{integer} X {i} = {integer * i}");
+                }
+            }
+            else if(multiplier > 10)
+            {
+                Console.WriteLine($"{integer} X {multiplier} = {integer * multiplier}");
+            }
+            
+        }
+    }
+}
