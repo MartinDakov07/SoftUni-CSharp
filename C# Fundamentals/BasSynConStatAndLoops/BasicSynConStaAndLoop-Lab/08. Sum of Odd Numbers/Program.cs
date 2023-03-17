f11af7f0e25508b47d210951cd09243fb6d13f43
Martin Dakov <martindakov6@gmail.com>
2023-03-17 22:47:54 +0200

    add files

---
+++ C# Fundamentals/BasSynConStatAndLoops/BasicSynConStaAndLoop-Lab/08. Sum of Odd Numbers/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _08._Sum_of_Odd_Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+            int oddSum = 0;
+            int oddNum = 1;
+            for (int i = 1; i <= num; i++)
+            {
+                
+                Console.WriteLine($"{oddNum}");
+                oddSum += oddNum;
+                oddNum += 2;
+            }
+            Console.WriteLine($"Sum: {oddSum}");
+        }
+    }
+}
