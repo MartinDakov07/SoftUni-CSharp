7803b315a9f16039b438bc6a9dbb3c39e286b61c
Martin Dakov <martindakov6@gmail.com>
2023-03-18 20:44:04 +0200

    add file

---
+++ C# Fundamentals/BasSynConStatAndLoops/BasicSynConStaAndLoop - Exercise/03. Vacation/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+
+namespace _03._Vacation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int people = int.Parse(Console.ReadLine());
+            string typeGroup = Console.ReadLine();
+            string day = Console.ReadLine();
+            double price = 0;
+            double totalPrice = 0;
+            if(typeGroup == "Students")
+            {
+                if(day == "Friday")
+                {
+                    price = 8.45;
+                }
+                else if(day == "Saturday")
+                {
+                    price = 9.80;
+                }
+                else if(day == "Sunday")
+                {
+                    price = 10.46;
+                }
+            }
+            else if (typeGroup == "Business")
+            {
+                if (day == "Friday")
+                {
+                    price = 10.90;
+                }
+                else if (day == "Saturday")
+                {
+                    price = 15.60;
+                }
+                else if (day == "Sunday")
+                {
+                    price = 16;
+                }
+            }
+            else if (typeGroup == "Regular")
+            {
+                if (day == "Friday")
+                {
+                    price = 15;
+                }
+                else if (day == "Saturday")
+                {
+                    price = 20;
+                }
+                else if (day == "Sunday")
+                {
+                    price = 22.50;
+                }
+            }
+            
+            if(typeGroup == "Students")
+            {
+                if(people >= 30)
+                {
+                    price = price - price * 0.15;
+                }
+            }
+            else if(typeGroup == "Business")
+            {
+                if(people >= 100)
+                {
+                    people -= 10;
+                }
+            }
+            else if(typeGroup == "Regular")
+            {
+                if (people >= 10 && people <= 20)
+                {
+                    price = price - price * 0.05;
+                }
+            }
+            totalPrice = price * people;
+            Console.WriteLine($"Total price: {totalPrice:f2}");
+        }
+    }
+}
