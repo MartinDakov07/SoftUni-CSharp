bb3b50ba84cc98b5c1fad6fef29690f7c7d9e3c3
Martin Dakov <martindakov6@gmail.com>
2023-03-18 20:47:17 +0200

    Learning deeper
    Basic Syntax, Conditional Statements and Loops

---
+++ C# Fundamentals/BasSynConStatAndLoops/BasicSynConStaAndLoop - Exercise/11. Orders/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace _11._Orders
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int orders = int.Parse(Console.ReadLine());
+            double priceCapsule = 0;
+            int days = 0;
+            int capsules = 0;
+            double totalPrice = 0;
+            double price = 0;
+            for(int i = 1; i <= orders; i++)
+            {
+                priceCapsule = 0;
+                days = 0;
+                capsules = 0;
+                price = 0;
+                priceCapsule = double.Parse(Console.ReadLine());
+                days = int.Parse(Console.ReadLine());
+                capsules = int.Parse(Console.ReadLine());
+                price = (days * capsules) * priceCapsule;
+                totalPrice += price;
+                Console.WriteLine($"The price for the coffee is: ${price:f2}");
+            }
+            Console.WriteLine($"Total: ${totalPrice:f2}");
+        }
+    }
+}
