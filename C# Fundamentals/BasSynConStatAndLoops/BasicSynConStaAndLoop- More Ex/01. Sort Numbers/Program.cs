6dfc52ec872dd6f349eefc4f7963796619caaad8
Martin Dakov <martindakov6@gmail.com>
2023-03-19 23:52:05 +0200

    add file

---
+++ C# Fundamentals/BasSynConStatAndLoops/BasicSynConStaAndLoop- More Ex/01. Sort Numbers/Program.cs
@@ -0,0 +1,22 @@
+﻿//Create a program that receives three real numbers and sorts them in descending order.
+//Print each number on a new line.
+using System;
+namespace _01._Sort_Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = int.Parse(Console.ReadLine());
+            int num2 = int.Parse(Console.ReadLine());
+            int num3 = int.Parse(Console.ReadLine());
+            int highest = Math.Max(Math.Max(num1, num2), Math.Max(num2, num3));
+            int lowest = Math.Min(Math.Min(num1, num2), Math.Min(num2, num3));
+            int average = num1 + num2 + num3 - lowest - highest;
+            Console.WriteLine(highest);
+            Console.WriteLine(average);
+            Console.WriteLine(lowest);
+           
+        }
+    }
+}
