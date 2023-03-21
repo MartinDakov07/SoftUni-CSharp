1490891e453d27eeb9579f686906668ae8f7edd8
Martin Dakov <martindakov6@gmail.com>
2023-03-21 22:44:02 +0200

    add file

---
+++ C# Fundamentals/DataTypesAndVariables/DataTypesAndVari-Lab/08. Town Info/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace _08._Town_Info
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = Console.ReadLine();
+            long population = long.Parse(Console.ReadLine());
+            ushort km = ushort.Parse(Console.ReadLine());
+            Console.WriteLine($"Town {name} has population of {population} and area {km} square km.");
+        }
+    }
+}
