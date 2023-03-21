e2dbceafc73dcbeebadeed75bad4c600dd64efa7
Martin Dakov <martindakov6@gmail.com>
2023-03-21 22:42:28 +0200

    add file

---
+++ C# Fundamentals/DataTypesAndVariables/DataTypesAndVari-Lab/02. Pounds to Dollars/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace _02._Pounds_to_Dollars
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            float pounds = float.Parse(Console.ReadLine());
+            float dollars = pounds * 1.31f;
+            Console.WriteLine($"{dollars:f3}");
+        }
+    }
+}
