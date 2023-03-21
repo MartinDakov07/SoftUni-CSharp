06a2b0b3b15a8f1579d84f6c0d3486a1bbc0bd8c
Martin Dakov <martindakov6@gmail.com>
2023-03-21 22:43:46 +0200

    add file

---
+++ C# Fundamentals/DataTypesAndVariables/DataTypesAndVari-Lab/07. Concat Names/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace _07._Concat_Names
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string firstName = Console.ReadLine();
+            string lastName = Console.ReadLine();
+            
+            object delimiter = Console.ReadLine();
+            
+            Console.WriteLine($"{firstName}{delimiter}{lastName}");
+            //int chNum = int.Parse(Console.ReadLine());
+            //Console.WriteLine($"Character of ur num is: {(char)chNum}");
+        }
+    }
+}
