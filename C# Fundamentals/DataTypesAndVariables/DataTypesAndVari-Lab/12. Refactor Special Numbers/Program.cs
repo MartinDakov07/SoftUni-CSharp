5eecaa4ff6e6df1cc9a68679b7cdb48031e13b31
Martin Dakov <martindakov6@gmail.com>
2023-03-21 22:45:22 +0200

    Data Types and Variables
    Still ez

---
+++ C# Fundamentals/DataTypesAndVariables/DataTypesAndVari-Lab/12. Refactor Special Numbers/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace _12._Refactor_Special_Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+
+            bool isSpecialNum = false;
+            
+            for (int i = 1; i <= num; i++)
+
+            {
+                int numCopy = 0;
+                int sum = 0;
+                numCopy = i;
+
+                while (numCopy > 0)
+
+                {
+
+                    sum += numCopy % 10;
+
+                    numCopy = numCopy / 10;
+
+                }
+
+                isSpecialNum = (sum == 5) || (sum == 7) || (sum == 11);
+
+                Console.WriteLine("{0} -> {1}", i, isSpecialNum);
+                               
+            }
+        }
+    }
+}
