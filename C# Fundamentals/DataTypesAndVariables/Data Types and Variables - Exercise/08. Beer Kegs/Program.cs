c6e0875da3da75571946f389b522b67e7dbdc56c
Martin Dakov <martindakov6@gmail.com>
2023-03-22 22:57:33 +0200

    add file

---
+++ C# Fundamentals/DataTypesAndVariables/Data Types and Variables - Exercise/08. Beer Kegs/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace _08._Beer_Kegs
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numKegs = int.Parse(Console.ReadLine());
+            string model = string.Empty;
+            double radius = 0;
+            int height = 0;
+            double topVolume = 0;
+            string topModel = string.Empty;
+            for(int i = 0;i < numKegs; i++)
+            {
+                radius = 0;
+                height = 0;
+                model = Console.ReadLine();
+                radius = double.Parse(Console.ReadLine());
+                height = int.Parse(Console.ReadLine());
+                double volume = (double)(Math.PI * (radius * radius) * height);
+                if(volume > topVolume)
+                {
+                    topVolume = volume;
+                    topModel = model;
+                }
+
+            }
+            Console.WriteLine($"{topModel}");
+            
+        }
+    }
+}
