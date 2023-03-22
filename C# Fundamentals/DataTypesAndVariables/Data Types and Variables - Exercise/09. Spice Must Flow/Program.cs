84f439b28e75fd37367b20745fd2ca442afe0d88
Martin Dakov <martindakov6@gmail.com>
2023-03-22 22:57:51 +0200

    add file

---
+++ C# Fundamentals/DataTypesAndVariables/Data Types and Variables - Exercise/09. Spice Must Flow/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace _09._Spice_Must_Flow
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int startYields = int.Parse(Console.ReadLine());
+            int leftSpices = 0;
+            int daysCount = 0;
+            if(startYields >= 100)
+            {
+                while (startYields >= 100)
+                {
+                    leftSpices += startYields - 26;
+                    if(leftSpices < 26)
+                    {
+                        leftSpices += 0;
+                    }
+                    daysCount++;
+                    startYields -= 10;
+                }
+                if (startYields < 100)
+                {
+                    leftSpices -= 26;
+                }
+                Console.WriteLine($"{daysCount}");
+                Console.WriteLine($"{leftSpices}");
+            }
+            else
+            {
+                Console.WriteLine($"{daysCount}");
+                Console.WriteLine($"{leftSpices}");
+            }
+            
+
+        }
+    }
+}
