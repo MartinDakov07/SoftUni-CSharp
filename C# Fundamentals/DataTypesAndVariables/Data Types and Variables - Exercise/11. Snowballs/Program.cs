46f56b355849869766b2cb84f3f7fc4952d322f2
Martin Dakov <martindakov6@gmail.com>
2023-03-22 22:58:20 +0200

    add file

---
+++ C# Fundamentals/DataTypesAndVariables/Data Types and Variables - Exercise/11. Snowballs/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Numerics;
+namespace _11._Snowballs
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int snowballs = int.Parse(Console.ReadLine());
+            
+            
+            BigInteger snowballBestValue = BigInteger.MinusOne;
+            int maxSnow = 0;
+            int maxTime = 0;
+            int maxQuality = 0;
+            for(int i = 0; i < snowballs; i++)
+            {
+                int snowballSnow = int.Parse(Console.ReadLine());
+                int snowballTime = int.Parse(Console.ReadLine());
+                int snowballQuality = int.Parse(Console.ReadLine());
+                BigInteger snowballValue = BigInteger.Pow((snowballSnow / snowballTime),snowballQuality);
+                if(snowballValue > snowballBestValue)
+                {
+                    snowballBestValue = snowballValue;
+                    maxSnow = snowballSnow;
+                    maxTime = snowballTime;
+                    maxQuality = snowballQuality;
+                }
+            }
+            Console.WriteLine($"{maxSnow} : {maxTime} = {snowballBestValue} ({ maxQuality})");
+        }
+    }
+}
