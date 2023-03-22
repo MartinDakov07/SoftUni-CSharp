a46e74e89c701863e0ca78b78c4bdadf42ea98bc
Martin Dakov <martindakov6@gmail.com>
2023-03-22 22:56:01 +0200

    add file

---
+++ C# Fundamentals/DataTypesAndVariables/Data Types and Variables - Exercise/05.PrintPartOfASCITab/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _05._Print_Part_Of_ASCII_Table
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int startIndex = int.Parse(Console.ReadLine());
+            int finalIndex = int.Parse(Console.ReadLine());
+            for(int i = startIndex; i <= finalIndex; i++)
+            {
+                char symbol = (char)i;
+                Console.Write($"{symbol} ");
+            }
+
+
+        }
+    }
+}
