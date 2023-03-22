ab06dea1f0c7a67320b09257ae4777f9f9b0c19d
Martin Dakov <martindakov6@gmail.com>
2023-03-22 22:54:01 +0200

    add file

---
+++ C# Fundamentals/DataTypesAndVariables/Data Types and Variables - Exercise/01. Integer Operations/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _01._Integer_Operations
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstNumber = int.Parse(Console.ReadLine());
+            int secondNumber = int.Parse(Console.ReadLine());
+            int thirdNumber = int.Parse(Console.ReadLine());
+            int fourthNumber = int.Parse(Console.ReadLine());
+            int firstOperation = firstNumber + secondNumber;
+            int secondOperation = (int)(firstOperation / thirdNumber);
+            int thirdOperation = secondOperation * fourthNumber;
+            Console.WriteLine(thirdOperation);
+
+        }
+    }
+}
