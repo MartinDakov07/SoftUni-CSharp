70f208c2761a048f20b27932c6fe070c8a3b4d3b
Martin Dakov <martindakov6@gmail.com>
2023-03-22 22:58:05 +0200

    add file

---
+++ C# Fundamentals/DataTypesAndVariables/Data Types and Variables - Exercise/10. Poke Mon/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace _10._Poke_Mon
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int m = int.Parse(Console.ReadLine());
+            int y = int.Parse(Console.ReadLine());
+            int pokedTargets = 0;
+            int maxN = n;
+            
+            while (n >= m)
+            {
+                n = n - m;
+                pokedTargets++;
+                if (n == maxN * 0.50)
+                {
+                    if(y!=0)
+                    {
+                        n = (int)(n / y);                                                
+                    }
+                    else
+                    {
+                        continue;
+                    }                   
+                }                              
+                                                   
+            }
+            Console.WriteLine($"{n}");
+            Console.WriteLine($"{pokedTargets}");
+        }
+    }
+}
