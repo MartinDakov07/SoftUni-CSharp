df164b4b8637d3ed9ef6349e6e14c17d779fe348
Martin Dakov <martindakov6@gmail.com>
2023-03-23 23:06:04 +0200

    add file

---
+++ C# Fundamentals/DataTypesAndVariables/Data Types and Variables - More Exercise/05. Decrypting Message/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace _05._Decrypting_Message
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int key = int.Parse(Console.ReadLine());
+            int n = int.Parse(Console.ReadLine());
+            string message = "";
+            for(int i = 0; i < n; i++)
+            {
+                char c = char.Parse(Console.ReadLine());
+                int cValue = (int)c;
+                int cNewValue = cValue + key;
+                char cKey = (char)cNewValue;
+                message += cKey;
+            }
+            Console.WriteLine(message);
+        }
+    }
+}
