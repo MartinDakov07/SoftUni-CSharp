1c5a3c217150c65b49e42629ab5f4b71382dbfd2
Martin Dakov <martindakov6@gmail.com>
2023-03-23 23:06:18 +0200

    add file

---
+++ C# Fundamentals/DataTypesAndVariables/Data Types and Variables - More Exercise/06. Balanced Brackets/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace _06._Balanced_Brackets
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            bool isBalanced = true;
+            bool openedBracket = false;
+            for(int i = 0; i < n; i++)
+            {
+                string randomString = Console.ReadLine();
+                if(randomString == "(")
+                {                                       
+                    if (openedBracket)
+                    {
+                        isBalanced = false;
+                    }
+                    else
+                    {
+                        openedBracket = true;
+                    }                  
+                }
+                else if(randomString == ")")
+                {
+                    if (openedBracket)
+                    {
+                        openedBracket = false;                       
+                    }
+                    else
+                    {
+                        isBalanced = false;
+                    }                  
+                }
+            }
+            if(openedBracket) isBalanced = false;
+            if (isBalanced)
+            {
+                Console.WriteLine("BALANCED");
+            }
+            else
+            {
+                Console.WriteLine("UNBALANCED");
+            }
+        }
+    }
+}
