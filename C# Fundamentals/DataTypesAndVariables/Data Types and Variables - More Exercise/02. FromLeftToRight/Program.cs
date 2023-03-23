24ffa786e80ec5245ffb0033dd008bc4316b635f
Martin Dakov <martindakov6@gmail.com>
2023-03-23 23:03:54 +0200

    add file

---
+++ C# Fundamentals/DataTypesAndVariables/Data Types and Variables - More Exercise/02. FromLeftToRight/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+namespace _02._From_Left_to_The_Right
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numOfRows = int.Parse(Console.ReadLine());          
+            for(int i = 0; i < numOfRows; i++)
+            {
+                int sum = 0;
+                string input = Console.ReadLine();
+                long[] nums = input.Split().Select(long.Parse).ToArray();
+                long biggerNum = nums[0] >= nums[1] ? nums[0] : nums[1];
+                string numString = biggerNum.ToString();
+                int position = numString[0] == '-' ? 1 : 0;
+                for(int j = position; j < numString.Length; j++)
+                {
+                    sum += int.Parse(numString[j].ToString());
+                }
+                Console.WriteLine(sum);
+            }
+        }
+    }
+}
