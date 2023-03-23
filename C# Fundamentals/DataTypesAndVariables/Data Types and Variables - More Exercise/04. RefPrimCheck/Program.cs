a86fd5caca5c9d44a5b02baeef5e886c27bdc20a
Martin Dakov <martindakov6@gmail.com>
2023-03-23 23:05:40 +0200

    add file

---
+++ C# Fundamentals/DataTypesAndVariables/Data Types and Variables - More Exercise/04. RefPrimCheck/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace _04._Refactoring_Prime_Checker
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 2; i <= n; i++)
+            {
+                bool IsPrime = true;
+                for (int j = 2; j < i; j++)
+                {
+                    if (i % j == 0)
+                    {
+                        IsPrime = false;
+                        break;
+                    }
+                }
+                Console.WriteLine("{0} -> {1}", i, IsPrime);
+            }
+
+        }
+    }
+}
