98180f22d1c2dced5500fa8c550943b0e088f9d1
Martin Dakov <martindakov6@gmail.com>
2023-03-26 23:51:32 +0300

    add files

---
+++ C# Fundamentals/Arrays/Arrays - Lab/P06. Even and Odd Subtraction/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+namespace P06._Even_and_Odd_Subtraction
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine()
+                .Split(' ')
+                .Select(int.Parse)
+                .ToArray();
+            int oddSum = 0;
+            int evenSum = 0;
+            for(int i = 0; i < numbers.Length; i++)
+            {
+                if (numbers[i] % 2 == 0)
+                {
+                    evenSum+=numbers[i];
+                }
+                else
+                {
+                    oddSum += numbers[i];
+                }
+            }
+            int difference = evenSum - oddSum;
+            Console.WriteLine(difference);
+        }
+    }
+}
