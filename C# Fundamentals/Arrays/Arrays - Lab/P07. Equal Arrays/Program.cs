5f083443f71d903e81efb43c4f72a1acba4a0ae1
Martin Dakov <martindakov6@gmail.com>
2023-03-26 23:51:57 +0300

    add files
    If you learn arrays you are god

---
+++ C# Fundamentals/Arrays/Arrays - Lab/P07. Equal Arrays/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Linq;
+namespace P07._Equal_Arrays
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr1 = Console.ReadLine()
+                .Split(' ')
+                .Select(int.Parse)
+                .ToArray();
+            int[] arr2 = Console.ReadLine()
+                .Split(' ')
+                .Select(int.Parse)
+                .ToArray();
+            int sum1 = 0;
+            int sum2 = 0;
+            for(int i = 0;i < arr1.Length; i++)
+            {
+                sum1 += arr1[i];
+                sum2 += arr2[i];
+                if (arr1[i] != arr2[i])
+                {
+                    Console.WriteLine($"Arrays are not identical. Found difference at {i} index");
+                    break;
+                }                
+            }
+            if(sum1 == sum2)
+            {
+                Console.WriteLine($"Arrays are identical. Sum: {sum1}");
+            }
+        }
+    }
+}
---
+++ C# Fundamentals/Arrays/Arrays - Lab/P08. Condense Array to Number/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Linq;
+namespace P08._Condense_Array_to_Number
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine()
+                .Split(' ')
+                .Select(int.Parse)
+                .ToArray();
+            int[] condensed = new int[numbers.Length-1];
+            while(numbers.Length > 1)
+            {
+                for (int i = 0; i < numbers.Length - 1; i++)
+                {
+                    condensed[i] = numbers[i] + numbers[i + 1];
+                    if(i == condensed.Length - 1)
+                    {
+                        numbers = condensed;
+                        condensed = new int [numbers.Length - 1];
+                    }
+                    
+                }
+            }
+           
+            Console.WriteLine(numbers[0]);
+            //int[] nums = Console.ReadLine().Split(" ").Select(int.Parse).ToArray();
+            //int[] condenced = new int[nums.Length - 1];
+            //
+            //while (nums.Length > 1)
+            //{
+            //    for (int i = 0; i < nums.Length - 1; i++)
+            //    {
+            //        condenced[i] = nums[i] + nums[i + 1];
+            //        if (i == condenced.Length - 1)
+            //        {
+            //            nums = condenced;
+            //            condenced = new int[nums.Length - 1];
+            //        }
+            //    }
+            //}
+            //Console.WriteLine(nums[0]); //nums.Length = 1 -> index = 0 
+
+        }
+    }
+}
