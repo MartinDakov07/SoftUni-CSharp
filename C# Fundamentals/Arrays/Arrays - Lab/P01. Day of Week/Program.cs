ef57a16f16dcd1cbc77f91bfd2859ae53cd37e18
Martin Dakov <martindakov6@gmail.com>
2023-03-26 23:50:33 +0300

    add file

---
+++ C# Fundamentals/Arrays/Arrays - Lab/P01. Day of Week/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace P01._Day_of_Week
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string[] daysOfWeek = new string[7]
+            {
+
+                "Monday",
+                "Tuesday",
+                "Wednesday",
+                "Thursday",
+                 "Friday",
+                 "Saturday",
+                 "Sunday"
+            };
+            int day = int.Parse(Console.ReadLine());
+            if(day > 7 || day < 1)
+            {
+                Console.WriteLine("Invalid day!");
+                return;
+            }
+            Console.WriteLine(daysOfWeek[day-1]);
+
+        }
+    }
+}
