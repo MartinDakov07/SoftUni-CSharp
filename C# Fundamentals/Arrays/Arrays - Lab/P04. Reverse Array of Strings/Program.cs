c49b91e193e8fe67f3cf693c27626342b6db2dde
Martin Dakov <martindakov6@gmail.com>
2023-03-26 23:51:15 +0300

    add files

---
+++ C# Fundamentals/Arrays/Arrays - Lab/P04. Reverse Array of Strings/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+namespace P04._Reverse_Array_of_Strings
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var symbols = Console.ReadLine()
+                .Split()               
+                .ToArray();
+            for(int i = symbols.Length - 1; i >= 0; i--)
+            {
+                Console.Write($"{symbols[i]} ");
+            }
+
+        }
+    }
+}
