4fae193ea16c690f7c893d2b25b1238f701c841f
Martin Dakov <martindakov6@gmail.com>
2023-03-27 23:04:50 +0300

    add file

---
+++ C# Fundamentals/Arrays/Arrays - Ex/06. Equal Sum/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Linq;
+namespace _06._Equal_Sum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine()
+            .Split(' ')
+            .Select(int.Parse)
+            .ToArray();
+            if(numbers.Length == 1)
+            {
+                Console.WriteLine("0");
+                return;
+            }
+            int leftSum = 0;
+            int rightSum = 0;
+            for (int i = 0; i < numbers.Length; i++)
+            {
+            
+                rightSum = 0;
+                for (int j = i+1; j < numbers.Length; j++)
+                {
+                    rightSum += numbers[j];
+                }
+                leftSum = 0;
+                for(int k = i - 1; k>=0; k--)
+                {
+                    leftSum += numbers[k];
+                }
+                if(leftSum == rightSum)
+                {
+                    Console.WriteLine(i);
+                    return;
+                }
+            }
+            
+            
+              Console.WriteLine("no");
+
+        }
+    }
+}
