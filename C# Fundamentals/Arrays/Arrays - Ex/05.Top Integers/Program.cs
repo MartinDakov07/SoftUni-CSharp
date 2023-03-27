af25fe7da3959007c7aa945c4de8f29e5018277b
Martin Dakov <martindakov6@gmail.com>
2023-03-27 23:04:37 +0300

    add file

---
+++ C# Fundamentals/Arrays/Arrays - Ex/05.Top Integers/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+namespace _05.Top_Integers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                int currNum = numbers[i];
+                bool isTopInt = true;
+                for (int j = i + 1; j < numbers.Length; j++)
+                {
+                    int nextNum = numbers[j];
+                    if (nextNum >= currNum)
+                    {
+                        isTopInt = false;
+                        break;
+                    }
+                }
+                if (isTopInt)
+                {
+                    Console.Write(currNum + " ");
+                }
+            }
+        }
+    }
+}
