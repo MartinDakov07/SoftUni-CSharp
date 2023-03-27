76feae8be2519420ea03f03e1a4f8445b0dfd333
Martin Dakov <martindakov6@gmail.com>
2023-03-27 23:03:55 +0300

    add file

---
+++ C# Fundamentals/Arrays/Arrays - Ex/02.Common Elements/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Linq;
+namespace _02.Common_Elements
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] firstArray = Console.ReadLine().Split(" ");
+            string[] secondArray = Console.ReadLine().Split(" ");          
+            for (int i = 0; i < secondArray.Length; i++)
+            {
+                for (int v = 0; v < firstArray.Length; v++)
+                {
+                    if (secondArray[i] == firstArray[v])
+                    {
+                        Console.Write($"{secondArray[i]} ");
+
+                    }
+                }
+            }
+            //My project
+            //string[] elements = Console.ReadLine()
+            //    .Split(" ");
+            //
+            //string[] elements2 = Console.ReadLine()
+            //    .Split(" ");
+            //
+            //string[] forPrint = new string[elements.Length];
+            //    
+            //    
+            //for (int i = 0; i < elements2.Length; i++)
+            //{
+            //    for(int j = 0; j < elements.Length; j++)
+            //    {
+            //        if (elements2[i]== elements[j])
+            //        {
+            //            Console.Write(String.Join(' '), elements2[i]);
+            //        }
+            //    }
+            //    
+            //}
+
+
+        }
+    }
+}
