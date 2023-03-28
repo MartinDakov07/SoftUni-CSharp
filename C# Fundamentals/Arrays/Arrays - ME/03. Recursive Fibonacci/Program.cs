d299a35ac458ade4597c002bc7aff5ebed853cdd
Martin Dakov <martindakov6@gmail.com>
2023-03-28 23:22:57 +0300

    add file

---
+++ C# Fundamentals/Arrays/Arrays - ME/03. Recursive Fibonacci/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Numerics;
+namespace _03._Recursive_Fibonacci
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            int n = int.Parse(Console.ReadLine());       
+            Console.WriteLine(GetFibonacci(n));     
+        }
+        static int GetFibonacci(int n)
+        {
+            if (n == 2 || n == 1) return 1;
+            int number = GetFibonacci(n - 1) + GetFibonacci(n - 2);
+            return number;
+        }
+    }
+}
