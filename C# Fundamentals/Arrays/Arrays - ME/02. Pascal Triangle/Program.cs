a7afb4ca3d150380dba9a887e572504d3e2796bf
Martin Dakov <martindakov6@gmail.com>
2023-03-28 23:22:38 +0300

    add file

---
+++ C# Fundamentals/Arrays/Arrays - ME/02. Pascal Triangle/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+namespace _02._Pascal_Triangle
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int[] newArray = new int[n + 1];
+            int[] oldArray = new int[n + 1];
+            newArray[1] = 1;
+            Console.WriteLine(newArray[1]);
+            for(int rows = 1; rows < n; rows++)
+            {
+                for (int i = 1; i <= n; i++) oldArray[i] = newArray[i];
+                
+                for(int i = 1; i <= rows + 1; i++)
+                {
+                    newArray[i] = oldArray[i - 1] + oldArray[i];
+                    Console.Write(newArray[i] + " ");
+                }
+                Console.WriteLine();                
+            }
+        }
+    }
+}
