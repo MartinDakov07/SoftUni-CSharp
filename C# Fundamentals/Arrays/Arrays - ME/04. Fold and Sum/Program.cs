5280ccfd872b6588a0905064ae93a47404bfbe98
Martin Dakov <martindakov6@gmail.com>
2023-03-28 23:23:16 +0300

    add file

---
+++ C# Fundamentals/Arrays/Arrays - ME/04. Fold and Sum/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Numerics;
+namespace _04._Fold_and_Sum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine().Split(' ',StringSplitOptions.RemoveEmptyEntries).
+                Select(int.Parse).   
+                ToArray();
+            int k = numbers.Length / 4;
+            int[] upperRow = new int[2 * k];
+            int[] lowerRow = new int[2 * k];
+
+            int counter = 0;
+            for (int i = k; i < k * 3; i++) lowerRow[counter++] = numbers[i];
+            counter = 0;
+            for(int i = k - 1; i >= 0; i--) upperRow[counter++] = numbers[i];
+            for(int i = 4 * k - 1; i >= 3 * k; i--) upperRow[counter++] = numbers[i];
+
+            for(int i = 0; i < lowerRow.Length; i++)
+            {
+                Console.Write(lowerRow[i] + upperRow[i] + " ");
+            }
+        }
+    }
+}
