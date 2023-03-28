0ea2ebdd9ec05751c86b25326a56d33a43919cbf
Martin Dakov <martindakov6@gmail.com>
2023-03-28 23:22:22 +0300

    add file

---
+++ C# Fundamentals/Arrays/Arrays - ME/01. Encrypt, Sort and Print Array/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+namespace _01._Encrypt__Sort_and_Print_Array
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            string[] names = new string[n];
+            int[] namesValue = new int[n];
+            for(int i = 0; i <= names.Length - 1; i++)
+            {
+                string name = Console.ReadLine();
+                int nameValue = 0;
+                foreach(char c in name)
+                {
+                    if(c == 'a' || c == 'e' || c == 'o' || c == 'i' || c == 'u'|| c == 'A' || c == 'E'|| c == 'O' || c == 'I' || c == 'U')
+                        
+                    {
+                        nameValue += (int)c * name.Length;
+                    }
+                    else
+                    {
+                        nameValue += (int)c / name.Length;
+                    }                  
+                }
+                names[i] = name;
+                namesValue[i] = nameValue;
+            }          
+            namesValue = namesValue.OrderBy(x => x).ToArray();
+            for(int i = 0; i <= names.Length - 1; i++)
+            {
+                Console.WriteLine(namesValue[i]);
+            }
+        }
+    }
+}
