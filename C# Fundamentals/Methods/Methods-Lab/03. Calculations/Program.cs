cb417b97b23d2caef2b8e3e466e3d1012d768d78
Martin Dakov <martindakov6@gmail.com>
2023-03-29 22:48:00 +0300

    add file

---
+++ C# Fundamentals/Methods/Methods-Lab/03. Calculations/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace _03._Calculations
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string command = Console.ReadLine();
+            int num1 = int.Parse(Console.ReadLine());
+            int num2 = int.Parse(Console.ReadLine());
+            if(command == "add")
+            {
+                AddCommand(num1, num2);
+            }
+            else if(command == "subtract")
+            {
+                 SubtractCommand(num1, num2);
+            }
+            else if(command == "multiply")
+            {
+                MiltiplyCommand(num1, num2);
+            }
+            else if(command == "divide")
+            {
+                DivideCommand(num1, num2);
+            }
+        }
+        static void AddCommand(int num1, int num2)
+        {
+            Console.WriteLine($"{num1+num2}");
+        }
+        static void SubtractCommand(int num1, int num2)
+        {
+            Console.WriteLine(num1-num2);
+        }
+        static void MiltiplyCommand(int num1, int num2)
+        {
+            Console.WriteLine(num1*num2);
+        }
+        static void DivideCommand(int num1, int num2)
+        {
+            Console.WriteLine(num1/num2);
+        }
+    }
+}
