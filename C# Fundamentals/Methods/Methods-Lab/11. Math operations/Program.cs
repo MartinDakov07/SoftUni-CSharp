cc80fd7425a9eabb54ecc6a296382f0d51da90f4
Martin Dakov <martindakov6@gmail.com>
2023-03-30 23:05:47 +0300

    add file

---
+++ C# Fundamentals/Methods/Methods-Lab/11. Math operations/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace _11._Math_operations
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = int.Parse(Console.ReadLine());
+            string @operator = Console.ReadLine();
+            int b = int.Parse(Console.ReadLine());
+            double result = Operations(a,b,@operator);
+            Console.WriteLine(result);
+        }
+          private static double Operations(int a,int b,string @operator)
+          {
+            
+            double result = 0;
+            if(@operator == "/")
+            {
+                result = a / b;
+            }
+            else if(@operator == "-")
+            {
+                result = a - b;
+            }
+            else if(@operator == "*")
+            {
+                result = a * b;
+            }
+            else if(@operator == "+")
+            {
+                result = a + b;
+            }
+            return result;
+          }
+    }
+}
