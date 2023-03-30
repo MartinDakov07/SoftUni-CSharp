7e20bef9e1edf1dba3aecdaa58ba6dff7289fd60
Martin Dakov <martindakov6@gmail.com>
2023-03-30 23:04:48 +0300

    add file

---
+++ C# Fundamentals/Methods/Methods-Lab/07. Repeat String/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace _07._Repeat_String
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string word = Console.ReadLine();
+            int count = int.Parse(Console.ReadLine());
+            string result = RepeatingWord(word, count); 
+            
+            Console.WriteLine(result);
+        }
+        static string RepeatingWord(string word, int count)
+        {
+            string result = "";
+            for(int i = 0; i < count; i++)
+            {
+                result += word;
+            }
+            return result;
+        }
+    }
+}
