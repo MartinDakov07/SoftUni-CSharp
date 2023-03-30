5d2c254f4214773276a5c626509908765ece0e52
Martin Dakov <martindakov6@gmail.com>
2023-03-30 23:05:02 +0300

    add file

---
+++ C# Fundamentals/Methods/Methods-Lab/08. Math Power/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace _08._Math_Power
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int bace = int.Parse(Console.ReadLine());
+            int power = int.Parse(Console.ReadLine());
+            double result = RaisePower(bace, power);
+            Console.WriteLine(result);
+        }
+        static double RaisePower(int bace, int power)
+        {
+            double result = 1;
+            result = Math.Pow(bace ,power);
+            //for(int i = 1; i <= power; i++)
+            //{
+            //    result *= bace;
+            //}
+            return result;
+        }
+    }
+}
