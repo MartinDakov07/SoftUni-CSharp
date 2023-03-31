6e1172fb9027972226fec3c688f82b0189e5ed5c
Martin Dakov <martindakov6@gmail.com>
2023-03-31 22:42:39 +0300

    add file

---
+++ C# Fundamentals/Methods/Methods-Ex/01. Smallest of Three Numbers/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace _01._Smallest_of_Three_Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            int c = int.Parse(Console.ReadLine());
+            int smallestNum = GetSmallestNum(a, b, c);
+            Console.WriteLine(smallestNum);
+        }
+        static int GetSmallestNum(int a , int b , int c)
+        {
+            int smallestNum = 0;
+            if (a < b && a < c)
+            {
+                smallestNum = a;
+            }
+            else if (b < a && b < c)
+            {
+                smallestNum = b;
+            }
+            else 
+            {
+                smallestNum = c;
+            }
+            return smallestNum;
+        }
+    }
+}
