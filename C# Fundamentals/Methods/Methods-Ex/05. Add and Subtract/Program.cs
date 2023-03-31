8634cb3a40454537c600d4d26c49e60486b1ea24
Martin Dakov <martindakov6@gmail.com>
2023-03-31 22:43:50 +0300

    add file

---
+++ C# Fundamentals/Methods/Methods-Ex/05. Add and Subtract/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace _05._Add_and_Subtract
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstNum = int.Parse(Console.ReadLine());
+            int secondNum = int.Parse(Console.ReadLine());
+            int thirdNum = int.Parse(Console.ReadLine());
+            int result = ResultAfterSubtract(thirdNum, firstNum, secondNum);
+            Console.WriteLine(result);
+
+        }
+        static int SumOfFirstTwoNums(int firstNum, int secondNum)
+        {
+            return firstNum + secondNum;
+        }
+        static int ResultAfterSubtract(int thirdNum,int firstNum, int secondNum)
+        {
+            int firstAction = SumOfFirstTwoNums(firstNum, secondNum);
+            int secondAction = firstAction - thirdNum;
+            return secondAction;
+        }
+    }
+}
