84dccdcd12bb8fb38de8fe5ab27631bec1a9f875
Martin Dakov <martindakov6@gmail.com>
2023-03-31 22:42:53 +0300

    add file

---
+++ C# Fundamentals/Methods/Methods-Ex/02. Vowels Count/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace _02._Vowels_Count
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string word = Console.ReadLine();
+            int vowelsCount = GetVowelsFromWord(word);
+            Console.WriteLine(vowelsCount);
+        }
+        static int GetVowelsFromWord(string word)
+        {
+            int vowelsCount = 0;
+            for(int i = 0; i < word.Length; i++)
+            {
+                if(word[i] == 'a' || word[i] == 'e'|| word[i] == 'i'|| word[i] == 'o'|| word[i] == 'u'
+                    ||word[i] == 'A' || word[i] == 'E' ||
+                    word[i] == 'I' || word[i] == 'O' || word[i] == 'U')
+                {
+                    vowelsCount++;
+                }
+            }
+            return vowelsCount;
+        }
+    }
+}
