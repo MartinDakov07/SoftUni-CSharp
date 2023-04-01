ba9a4985e3133671f08e36d31c32ab8090785de8
Martin Dakov <martindakov6@gmail.com>
2023-04-01 23:53:34 +0300

    add file

---
+++ C# Fundamentals/Methods/Methods-Ex/09. Palindrome Integers/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace _09._Palindrome_Integers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string command = Console.ReadLine();
+            IsNumPalinDrome(command);
+        }
+        static void IsNumPalinDrome(string command)
+        {
+            
+            while(command != "END")
+            {
+                int currNum = int.Parse(command);
+                int numCopy = currNum;
+                
+                int result = 0;
+                while(numCopy > 0)
+                {
+                    result = result * 10 + numCopy % 10;
+                    numCopy /= 10;
+                }
+                if(currNum == result)
+                {
+                    Console.WriteLine("true");
+                }
+                else
+                {
+                    Console.WriteLine("false");
+                }
+                command = Console.ReadLine();
+                
+            }
+        }
+    }
+}
