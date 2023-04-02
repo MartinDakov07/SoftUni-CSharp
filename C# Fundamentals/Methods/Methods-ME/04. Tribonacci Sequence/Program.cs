f3b9560c9ab33e05bf42199d1b9fb59e63b4c1af
Martin Dakov <martindakov6@gmail.com>
2023-04-02 23:29:11 +0300

    add file

---
+++ C# Fundamentals/Methods/Methods-ME/04. Tribonacci Sequence/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+namespace _04._Tribonacci_Sequence
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+            if (num <= 0)
+            {
+                Console.Write("0");
+            }
+            else if(num == 1)
+            {
+                Console.Write("1");
+            }
+            else if(num == 2)
+            {
+                Console.Write("1 1");
+            }
+            else if(num == 3)
+            {
+                Console.Write("1 1 2");
+            }
+            else
+            {
+                Console.Write("1 1 2 ");
+                GetTribonacci(num);
+            }
+        }
+        static void GetTribonacci(int num)
+        {
+            int a = 1;
+            int b = 1;
+            int c = 2;
+
+            int result = num;
+            for(int i = 0; i < num - 3; i++)
+            {
+                result = a + b + c;
+                a = b;
+                b = c;
+                c = result;
+                Console.Write($"{c} ");
+            }
+            
+        }
+    }
+}
