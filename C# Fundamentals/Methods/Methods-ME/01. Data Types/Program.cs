c5e099eb29c1aa697b80028d21e91a0f712a1511
Martin Dakov <martindakov6@gmail.com>
2023-04-02 23:28:42 +0300

    add file

---
+++ C# Fundamentals/Methods/Methods-ME/01. Data Types/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Numerics;
+namespace _01._Data_Types
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string typeOfString = Console.ReadLine();
+            if(typeOfString == "int")
+            {
+                ReturnIntValue(typeOfString);
+            }
+            else if(typeOfString == "real")
+            {
+                ReturnRealValue(typeOfString);
+            }
+            else
+            {
+                ReturnStringValue(typeOfString);
+            }
+            
+        }
+        static void ReturnIntValue(string typeOfString)
+        {                      
+           int value = int.Parse(Console.ReadLine());
+            value = value * 2;
+            Console.WriteLine(value);
+        }
+        static void ReturnRealValue(string typeOfString)
+        {
+            double value = double.Parse(Console.ReadLine());
+            value = value * 1.5;
+            Console.WriteLine($"{value:f2}");
+        }
+        static void ReturnStringValue(string typeOfString)
+        {
+            string value = Console.ReadLine();
+            Console.WriteLine($"${value}$");
+        }
+
+    }
+}
