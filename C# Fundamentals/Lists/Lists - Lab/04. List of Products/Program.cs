12ced2ae618acea57191413ef54ef53d320d75e8
Martin Dakov <martindakov6@gmail.com>
2023-04-03 22:54:38 +0300

    add file

---
+++ C# Fundamentals/Lists/Lists - Lab/04. List of Products/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+namespace _04._List_of_Products
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numOfProduct = int.Parse(Console.ReadLine());
+            List<string> products = new List<string>();
+                
+            for(int i = 0; i < numOfProduct; i++)
+            {
+                string currentProduct = Console.ReadLine();
+                products.Add(currentProduct);
+            }
+            products.Sort();
+            for(int i = 0; i < products.Count; i++)
+            {
+                Console.WriteLine($"{i+1}.{products[i]}");//if i = 0 -> i+1 = 1 (first time reading)
+            }
+        }
+            
+    }
+}
