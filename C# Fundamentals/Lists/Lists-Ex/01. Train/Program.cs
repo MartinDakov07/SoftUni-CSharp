cdb5f6f841bfeb21bc64ed2402ca74f71248897b
Martin Dakov <martindakov6@gmail.com>
2023-04-04 23:16:04 +0300

    add file

---
+++ C# Fundamentals/Lists/Lists-Ex/01. Train/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+namespace _01._Train
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           List<int> wagons = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToList();
+            int maxPassangers = int.Parse(Console.ReadLine());
+            string commands = Console.ReadLine();
+            while(commands != "end")
+            {
+                string[] command = commands.Split();
+                if(command[0] == "Add")
+                {
+                    int numberToAdd = int.Parse(command[1]);
+                    wagons.Add(numberToAdd);
+                }
+                else
+                {
+                    int numberToInsert = int.Parse(command[0]);
+                    int num = maxPassangers;
+                    for(int i = 0; i < wagons.Count; i++)
+                    {
+                        if(wagons[i] + numberToInsert <=num)
+                        {
+                            wagons[i] += numberToInsert;
+                            break;
+                        }
+                    }
+                }
+                commands = Console.ReadLine();
+            }
+            Console.WriteLine(String.Join(" ", wagons));
+
+
+        }
+    }
+}
