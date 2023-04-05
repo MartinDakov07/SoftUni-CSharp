badb61798a4a075d69183a326f5de36df2077624
Martin Dakov <martindakov6@gmail.com>
2023-04-05 23:04:48 +0300

    add file

---
+++ C# Fundamentals/Lists/Lists-Ex/07. Append Arrays/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+namespace _07._Append_Arrays
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            
+            List<string> firstList = input
+                .Split('|', StringSplitOptions.RemoveEmptyEntries)
+                .Reverse()
+                .ToList();
+            List<int> secondList = new List<int>();
+            foreach(string numbers in firstList)
+            {
+                secondList.AddRange(numbers.Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToList());
+            }
+            Console.WriteLine(string.Join(" ", secondList));
+            
+
+
+        }
+    }
+}
