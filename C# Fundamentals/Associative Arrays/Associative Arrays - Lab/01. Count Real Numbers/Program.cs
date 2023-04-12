9209b3133d6d0d76fcaf0d6cc45591686f5fa037
Martin Dakov <martindakov6@gmail.com>
2023-04-12 23:23:00 +0300

    add file

---
+++ C# Fundamentals/Associative Arrays/Associative Arrays - Lab/01. Count Real Numbers/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+namespace _01._Count_Real_Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            SortedDictionary<double, int> occNumber = new SortedDictionary<double, int>();
+            double[] numbers = Console.ReadLine()
+                .Split(' ')
+                .Select(double.Parse)
+                .ToArray();
+            foreach(var number in numbers)
+            {
+                if (!occNumber.ContainsKey(number))
+                {
+                    occNumber.Add(number, 0);
+                }
+                occNumber[number]++;
+            }
+            foreach(var number in occNumber)
+            {
+                Console.WriteLine($"{number.Key} -> {number.Value}");
+            }
+          
+        }
+    }
+}
