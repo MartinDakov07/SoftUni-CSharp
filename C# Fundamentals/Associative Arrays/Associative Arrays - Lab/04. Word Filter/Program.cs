de0d8894db0d9ad380a644867f9a4f532d9a9bea
Martin Dakov <martindakov6@gmail.com>
2023-04-12 23:23:47 +0300

    add file

---
+++ C# Fundamentals/Associative Arrays/Associative Arrays - Lab/04. Word Filter/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+namespace _04._Word_Filter
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] words = Console.ReadLine()
+                  .Split(" ")
+                  .Where(w => w.Length  % 2 == 0)
+                  .ToArray();
+            
+            foreach(string word in words)
+            {
+                Console.WriteLine(word);
+            }
+            
+            
+        }
+    }
+}
