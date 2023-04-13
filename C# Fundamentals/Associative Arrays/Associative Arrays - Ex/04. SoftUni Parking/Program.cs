0e99ef6d018a8bb47a2bd0db8508cc3f3453fc32
Martin Dakov <martindakov6@gmail.com>
2023-04-13 23:31:31 +0300

    add file

---
+++ C# Fundamentals/Associative Arrays/Associative Arrays - Ex/04. SoftUni Parking/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+namespace _04._SoftUni_Parking
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, string> carInfo = new Dictionary<string, string>();
+            int n = int.Parse(Console.ReadLine());
+            for(int i = 0; i < n; i++)
+            {
+                string[] carArg = Console.ReadLine().Split(' ');
+                string command = carArg[0];
+                string name = carArg[1];
+                
+                if(command == "register")
+                {
+                    string license = carArg[2];
+                    if (!carInfo.ContainsKey(name) )
+                    {
+                        carInfo.Add(name, license);
+                        Console.WriteLine($"{name} registered {license} successfully");
+                    }
+                    
+                    else if (carInfo.ContainsKey(name) && carInfo.ContainsValue(license))
+                    {
+                        Console.WriteLine($"ERROR: already registered with plate number {license}");
+                    }
+                }
+               
+                if(command == "unregister")
+                {
+                    if (!carInfo.ContainsKey(name))
+                    {
+                        Console.WriteLine($"ERROR: user {name} not found");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"{name} unregistered successfully");
+                        carInfo.Remove(name);
+                    }
+                }
+            }
+            foreach(var car in carInfo)
+            {
+                Console.WriteLine($"{car.Key} => {car.Value}");
+            }
+        }
+    }
+}
