5b8a0be91eda01ed9bcacaca85a9c1dc66cb9f06
Martin Dakov <martindakov6@gmail.com>
2023-04-13 23:31:00 +0300

    add file

---
+++ C# Fundamentals/Associative Arrays/Associative Arrays - Ex/01. Count Chars in a String/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+namespace _01._Count_Chars_in_a_String
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<char, int> counts = new Dictionary<char, int>();
+            string[] words = Console.ReadLine()
+                .Split(" ")
+                .ToArray();
+            foreach(string c in words)
+            {
+                foreach(char ch in c)
+                {
+                    if (!counts.ContainsKey(ch))
+                    {
+                        counts.Add(ch, 0);
+                    }
+                    counts[ch]++;
+                }
+                
+            }
+            foreach(var count in counts)
+            {
+                Console.WriteLine($"{count.Key} -> {count.Value}");
+            }
+        }
+    }
+}
