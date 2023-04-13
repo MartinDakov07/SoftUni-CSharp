f0a68f28eed00a754f90c869d997bcf730f3ad9f
Martin Dakov <martindakov6@gmail.com>
2023-04-13 23:31:21 +0300

    add file

---
+++ C# Fundamentals/Associative Arrays/Associative Arrays - Ex/03. Orders/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+namespace _03._Orders
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, List<double>> products = new Dictionary<string, List<double>>();
+            string[] product = Console.ReadLine().Split();
+            while(product[0] != "buy")
+            {
+                
+                
+                string name = product[0];
+                double price = double.Parse(product[1]);
+                int quantity = int.Parse(product[2]);
+                
+                if (!products.ContainsKey(name))
+                {
+                    products.Add(name, new List<double>());
+                    products[name].Add(price);
+                    products[name].Add(quantity);
+                }
+                else
+                {
+                    products[name][1] += quantity;
+                    products[name][0] = price;
+                   
+                }
+                
+                product = Console.ReadLine().Split();
+            }
+            foreach(var item in products)
+            {
+                Console.WriteLine($"{item.Key} -> {(item.Value[0] * item.Value[1]):f2}");
+            }
+        }
+    }
+}
