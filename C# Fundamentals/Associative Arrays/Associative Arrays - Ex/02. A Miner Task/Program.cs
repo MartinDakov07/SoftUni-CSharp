9a05d911aa979705e9d3f6bda4b6154e307aa822
Martin Dakov <martindakov6@gmail.com>
2023-04-13 23:31:11 +0300

    add file

---
+++ C# Fundamentals/Associative Arrays/Associative Arrays - Ex/02. A Miner Task/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+namespace _02._A_Miner_Task
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, int> resources = new Dictionary<string, int>();
+            string resource = Console.ReadLine();
+            while (resource!= "stop")
+            {               
+                int quantity = int.Parse(Console.ReadLine());
+                if (!resources.ContainsKey(resource))
+                {
+                    resources.Add(resource, quantity);
+                }
+                else
+                {
+                    resources[resource] += quantity;
+                }
+                resource = Console.ReadLine();
+            }
+            foreach(var kvp in resources)
+            {
+                Console.WriteLine($"{kvp.Key} -> {kvp.Value}");
+            }
+        }
+    }
+}
