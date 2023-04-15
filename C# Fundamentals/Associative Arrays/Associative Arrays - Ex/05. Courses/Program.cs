f055fa04edf2dc58d8fc13ae256d15821ba1e3f3
Martin Dakov <martindakov6@gmail.com>
2023-04-15 22:52:07 +0300

    add file

---
+++ C# Fundamentals/Associative Arrays/Associative Arrays - Ex/05. Courses/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+namespace _05._Courses
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, List<string>> clubs = new Dictionary<string, List<string>>();
+            string[] club = Console.ReadLine().Split(" : ",StringSplitOptions.RemoveEmptyEntries);            
+            while(club[0] != "end")
+            {
+                string clubName = club[0];
+                string student = club[1];
+                if (!clubs.ContainsKey(clubName))
+                {
+                    clubs[clubName] = new List<string>();                   
+                }                               
+                clubs[clubName].Add(student);
+                club = Console.ReadLine().Split(" : ");
+            }
+            foreach(var kvp in clubs)
+            {
+                string clubName = kvp.Key;
+                List<string> students = kvp.Value;
+                Console.WriteLine($"{clubName}: {students.Count}");
+                foreach(string student in students)
+                {
+                    Console.WriteLine($"-- {student}");
+                }
+            }
+        }
+    }
+}
