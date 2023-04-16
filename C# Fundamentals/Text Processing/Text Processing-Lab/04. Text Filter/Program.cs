2f727c76b58eec3bbfc195a65aa701445e20c31e
Martin Dakov <martindakov6@gmail.com>
2023-04-16 23:35:18 +0300

    add file

---
+++ C# Fundamentals/Text Processing/Text Processing-Lab/04. Text Filter/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+using System.Numerics;
+using System.Collections.Generic;
+namespace _04._Text_Filter
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] wordsForBan = Console.ReadLine().Split(", ");
+            string text = Console.ReadLine();
+            foreach(var word in wordsForBan)
+            {
+                text = text.Replace(word, new string('*', word.Length));
+            }
+            Console.WriteLine(text);
+        }
+    }
+}
