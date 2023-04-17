b6c11d344befe30ae869992c8e5402043a54b188
Martin Dakov <martindakov6@gmail.com>
2023-04-17 22:46:06 +0300

    add file

---
+++ C# Fundamentals/Text Processing/Text Processing - Ex/02. Character Multiplier/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Collections.Generic;
+namespace _02._Character_Multiplier
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split();
+            GetStringSum(input[0], input[1]);
+        }
+       private static void GetStringSum(string inputOne, string inputTwo)
+       {
+            int sum = 0;
+            int minLength = Math.Min(inputOne.Length, inputTwo.Length);
+            for(int i = 0; i < minLength; i++)
+            {
+                sum += inputOne[i] * inputTwo[i];
+            }
+            string longerLength = inputOne;
+            if(longerLength.Length < inputTwo.Length)
+            {
+                longerLength = inputTwo;
+            }
+            for(int i = minLength; i < longerLength.Length; i++)
+            {
+                sum+=longerLength[i];
+            }
+            Console.WriteLine(sum);
+       }
+    }
+}
