6dc33f5de225978fa47d9997c941aa8414d41678
Martin Dakov <martindakov6@gmail.com>
2023-04-17 22:45:54 +0300

    add file

---
+++ C# Fundamentals/Text Processing/Text Processing - Ex/01. Valid Usernames/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+namespace _01._Valid_Usernames
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] names = Console.ReadLine().Split(", ");
+            foreach(string name in names)
+            {
+                if (name.Length > 3 && name.Length < 16)
+                {
+                    bool isUsernameValid = true;
+                    foreach(char c in name)
+                    {
+                        if(!(char.IsLetterOrDigit(c) || c == '_' || c == '-'))
+                        {
+                            isUsernameValid = false;
+                            break;
+                        }
+                    }
+                    if (isUsernameValid)
+                    {
+                        Console.WriteLine(name);
+                    }
+                }
+                
+            }
+        }
+    }
+}
