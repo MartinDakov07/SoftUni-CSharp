0d2162b67221ab722c18595337586a28d1b15b76
Martin Dakov <martindakov6@gmail.com>
2023-04-17 22:45:22 +0300

    add file

---
+++ C# Fundamentals/Text Processing/Text Processing-Lab/05. Digits, Letters and Other/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Numerics;
+namespace _05._Digits__Letters_and_Other
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine();
+            string letters = "";
+            string chars = "";
+            string digits = "";
+            foreach(char symbol in text)
+            {
+                if (char.IsLetter(symbol))
+                {
+                    letters += symbol;
+                }
+                else if (char.IsDigit(symbol))
+                {
+                    digits += symbol;
+                }
+                else 
+                {
+                    chars += symbol;
+                }
+            }
+            Console.WriteLine(digits);
+            Console.WriteLine(letters);
+            Console.WriteLine(chars);
+        }
+    }
+}
