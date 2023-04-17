ad6d5d8645db2a50f878f33b9b69903708e28fce
Martin Dakov <martindakov6@gmail.com>
2023-04-17 22:46:16 +0300

    add file

---
+++ C# Fundamentals/Text Processing/Text Processing - Ex/03. Extract File/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+namespace _03._Extract_File
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split("\\");
+            string file = input[input.Length - 1];
+            string[] fileInfo = file.Split(".");
+            string fileName = fileInfo[0];
+            string fileExtension = fileInfo[1];
+            Console.WriteLine($"File name: {fileName}");
+            Console.WriteLine($"File extension: {fileExtension}");
+
+        }
+    }
+}
