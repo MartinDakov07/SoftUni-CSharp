038009dc95237bae6664b0972851382be9696b9d
Martin Dakov <martindakov6@gmail.com>
2023-04-18 23:11:13 +0300

    add file

---
+++ C# Fundamentals/Text Processing/Text Processing - Ex/04. Caesar Cipher/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+namespace _04._Caesar_Cipher
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine();
+            string encryptedString = "";
+            ShiftLetter(text, encryptedString);
+        }
+        private static void ShiftLetter(string text, string encryptedString)
+        {
+            foreach(char currChar in text)
+            {
+                int currPosition = currChar;
+                currPosition += 3;
+                encryptedString += (char)currPosition;
+            }
+            Console.WriteLine(encryptedString);
+        }
+    }
+}
