f03e2a9a03acf9b3cb936e1d2c79b97991867613
Martin Dakov <martindakov6@gmail.com>
2023-04-25 22:48:13 +0300

    add file

---
+++ C# Fundamentals/Exam Preparations/Exam Preparation 4.0/02. Destination Mapper/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Text.RegularExpressions;
+using System.Collections.Generic;
+namespace _02._Destination_Mapper
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string pattern = @"(\/|\=)(?<place>[A-Z][A-Za-z]{2,})(\1)";
+            Regex regex = new Regex(pattern);
+            string input = Console.ReadLine();
+            MatchCollection matches = Regex.Matches(input, pattern);
+            int totalPoints = 0;
+            
+            List<string> matchItems = new List<string>();
+            if (matches.Count > 0)
+            {
+                foreach (Match match in matches)
+                {
+                    string kvp = match.Groups["place"].Value;
+                    matchItems.Add(kvp);
+                    totalPoints += match.Groups["place"].Value.Length;                  
+                }
+            }
+            Console.WriteLine($"Destinations: {string.Join(", ",matchItems)}");
+            Console.WriteLine($"Travel Points: {totalPoints}");
+        }
+    }
+}
