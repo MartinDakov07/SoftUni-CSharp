566e6843995c273d9d35dc87ae3001bdf9d671ab
Martin Dakov <martindakov6@gmail.com>
2023-04-27 22:51:06 +0300

    add file

---
+++ C# Fundamentals/Exam Preparations/Exam Preparation 6.0/01. Birthday Party/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Linq;
+using System.Numerics;
+namespace _01._Birthday_Party
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstEmployee = int.Parse(Console.ReadLine());
+            int secondEmployee = int.Parse(Console.ReadLine());
+            int thirdEmployee = int.Parse(Console.ReadLine());
+            int studentsCount = int.Parse(Console.ReadLine());
+            int hoursCount = 0;
+            int totalEmployee = firstEmployee + secondEmployee + thirdEmployee;
+            if(studentsCount == 0)
+            {
+                Console.WriteLine($"Time needed: 0h.");
+                return;
+            }
+            while(true)
+            {
+                hoursCount++;
+                
+                if (hoursCount % 4 == 0)
+                {                   
+                    continue;
+                }
+                studentsCount -= totalEmployee;
+                if (studentsCount <= 0)
+                {
+                    break;
+                }
+
+            }
+            Console.WriteLine($"Time needed: {hoursCount}h.");
+        }
+    }
+}
