53eebd0fb376326d21f09aecb27de675442740ff
Martin Dakov <martindakov6@gmail.com>
2023-04-29 22:51:29 +0300

    add file

---
+++ C# Fundamentals/RandomCodeFromSoftUni/First codes/01. Number Pyramid/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace _01._Number_Pyramid
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+            int currentNum = 1;
+            bool isBigger = false;
+            for(int rows = 1; rows <= num; rows++)
+            {
+                for(int cols = 1; cols <= rows; cols++)
+                {
+                    if(currentNum > num)
+                    {
+                        isBigger = true;
+                        break;
+                    }
+                    Console.Write(currentNum + " ");
+                    currentNum++;
+                }
+                if (isBigger)
+                {
+                    break;
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
