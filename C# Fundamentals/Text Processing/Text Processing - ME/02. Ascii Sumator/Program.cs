f47dd39cf706dd46137a64d8b6dc2cf62b5cea78
Martin Dakov <martindakov6@gmail.com>
2023-05-02 23:25:19 +0300

    add file

---
+++ C# Fundamentals/Text Processing/Text Processing - ME/02. Ascii Sumator/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+namespace _02._Ascii_Sumator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            char firstSymbol = char.Parse(Console.ReadLine());
+            char secondSymbol = char.Parse(Console.ReadLine());
+            string text = Console.ReadLine();
+            int sum = 0;
+            foreach(char c in text.Where(c=>c > (int)firstSymbol && c < (int)secondSymbol))
+            {
+                sum += (int)c;
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
