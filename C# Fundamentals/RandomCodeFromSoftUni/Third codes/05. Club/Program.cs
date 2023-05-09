be963ed3c93b8ab09ea052405aeab9a6b0c83b01
Martin Dakov <martindakov6@gmail.com>
2023-05-09 23:08:12 +0300

    add file

---
+++ C# Fundamentals/RandomCodeFromSoftUni/Third codes/05. Club/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace _05._Club
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double neededMoney = double.Parse(Console.ReadLine());
+            string coctail = Console.ReadLine();
+            int numCoctails = 0;
+            double price = 0;
+            double totalPrice = 0;
+            double coctailPrice = 0;
+            while(coctail != "Party!")
+            {
+                coctailPrice = 0;
+                price = 0;
+                numCoctails = int.Parse(Console.ReadLine());
+                price = coctail.Length;
+                coctailPrice = price * numCoctails;
+                if (coctailPrice % 2 != 0)
+                {
+                    coctailPrice = coctailPrice - coctailPrice * 0.25;
+                }
+                totalPrice += coctailPrice;
+                if(totalPrice >= neededMoney)
+                {
+                    break;
+                }
+                coctail = Console.ReadLine();
+            }
+            if(coctail == "Party!")
+            {
+                Console.WriteLine($"We need {(neededMoney - totalPrice):f2} leva more.");
+            }
+            else if(totalPrice >= neededMoney)
+            {
+                Console.WriteLine("Target acquired.");
+            }
+            Console.WriteLine($"Club income - {totalPrice:f2} leva.");
+        }
+    }
+}
