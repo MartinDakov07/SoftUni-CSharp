c83163fd2dd005b2e02ef266eb71973480c0db67
Martin Dakov <martindakov6@gmail.com>
2023-05-10 23:05:33 +0300

    add file

---
+++ C# Fundamentals/Regular Expressions/Regular Expressions - ME/4. Santa's Secret Helper/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+namespace _4._Santa_s_Secret_Helper
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int key = int.Parse(Console.ReadLine());
+            string secretMessage = Console.ReadLine();
+            List<string> goodChildren = new List<string>();
+            while(secretMessage != "end")
+            {
+                string message = String.Join("",secretMessage
+                    .ToCharArray()
+                    .Select(x=>(char)(x-key))
+                    .ToArray());
+                string regex = @"[@](?<name>[A-Za-z]+)[^@!:>-]+[!](?<behaviour>[G|N])[!]";
+                MatchCollection matches = Regex.Matches(message, regex);
+                if(matches.Count == 0)
+                {
+                    secretMessage = Console.ReadLine();
+                    continue;
+                }
+                string name = matches[0].Groups["name"].Value;
+                string behaviour = matches[0].Groups["behaviour"].Value;
+                if(behaviour == "G")
+                {
+                    goodChildren.Add(name);
+                }
+                secretMessage = Console.ReadLine();
+            }
+            Console.WriteLine(String.Join("\n",goodChildren));
+        }
+    }
+}
