041a222c64a5ad803eda611766d16f4b0fd6e519
Martin Dakov <martindakov6@gmail.com>
2023-05-11 22:59:37 +0300

    add file

---
+++ C# Fundamentals/RandomCodeFromSoftUni/Third codes/06. Renovation/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace _06._Renovation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int height = int.Parse(Console.ReadLine());
+            int width = int.Parse(Console.ReadLine());
+            int totalSize = height * width * 4;
+            double percent = double.Parse(Console.ReadLine()) / 100;
+            double wallPaint =Math.Ceiling (totalSize - totalSize * percent);
+            string paint = Console.ReadLine();
+            int paintUsed = 0;
+            while(paint != "Tired!")
+            {
+                paintUsed = 0;
+                paintUsed = int.Parse(paint);
+                wallPaint -= paintUsed;
+                if(wallPaint < 0)
+                {
+                    Console.WriteLine($"All walls are painted and you have {Math.Abs(wallPaint)} l paint left!");
+                    break;
+                }
+                if (wallPaint == 0)
+                {
+                    Console.WriteLine("All walls are painted! Great job, Pesho!");
+                    break;
+                }
+
+
+                paint = Console.ReadLine();
+            }
+            if(paint == "Tired!")
+            {
+                Console.WriteLine($"{wallPaint} quadratic m left.");
+          
+            }
+            
+           
+
+
+        }
+    }
+}
