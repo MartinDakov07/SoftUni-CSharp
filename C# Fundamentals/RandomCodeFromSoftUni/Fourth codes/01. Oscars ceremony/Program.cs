e4757f2e9aae16987e1515220dc18093a7c6a272
Martin Dakov <martindakov6@gmail.com>
2023-05-12 22:55:21 +0300

    add file

---
+++ C# Fundamentals/RandomCodeFromSoftUni/Fourth codes/01. Oscars ceremony/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace _01._Oscars_ceremony
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double rent = double.Parse(Console.ReadLine());
+            double statues = rent - rent * 0.3;
+            double ketyring = statues - statues * 0.15;
+            double sound = ketyring / 2;
+            double totalPrice = rent + statues + ketyring + sound;
+            Console.WriteLine($"{totalPrice:f2}");
+
+        }
+    }
+}
