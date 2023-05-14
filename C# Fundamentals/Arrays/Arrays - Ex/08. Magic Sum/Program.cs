311f9142f0098c9f38b3b112c48db08a4ad71bb7
Martin Dakov <martindakov6@gmail.com>
2023-05-14 23:37:29 +0300

    add file

---
+++ C# Fundamentals/Arrays/Arrays - Ex/08. Magic Sum/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+namespace _08._Magic_Sum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] numbers = Console.ReadLine()
+                .Split();                
+            int result = int.Parse(Console.ReadLine());
+            
+            for(int i = 0; i < numbers.Length; i++)
+            {
+                string[] uniqueNumbers = new string[2];
+                for (int j = i+1;j < numbers.Length; j++)
+                {
+                    if(int.Parse(numbers[i]) + int.Parse(numbers[j]) == result)
+                    {
+                        if(uniqueNumbers.Contains(numbers[i]) && uniqueNumbers.Contains(numbers[j]))
+                        {
+                            break;
+                        }
+                        else
+                        {
+                            uniqueNumbers[0]=numbers[i];
+                            uniqueNumbers[1]=numbers[j];
+                            Console.WriteLine($"{uniqueNumbers[0]} {uniqueNumbers[1]}");
+                        }
+                    }
+                }
+                
+            }
+        }
+    }
+}
