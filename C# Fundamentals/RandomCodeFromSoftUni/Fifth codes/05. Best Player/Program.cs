d2d5e920e729ed16d25ac4a948d9dd7fe81e3f10
Martin Dakov <martindakov6@gmail.com>
2023-05-16 22:58:16 +0300

    add file

---
+++ C# Fundamentals/RandomCodeFromSoftUni/Fifth codes/05. Best Player/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace _05._Best_Player
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string player = Console.ReadLine();
+            int maxGoal = 0;
+            string bestPlayer = "";
+            while(player != "END")
+            {
+                int goals = int.Parse(Console.ReadLine());
+                if(maxGoal < goals)
+                {
+                    bestPlayer = player;
+                    maxGoal = goals;
+                }
+                if(goals >= 10)
+                {
+                    break;
+                }
+                player = Console.ReadLine();
+            }
+            Console.WriteLine($"{bestPlayer} is the best player!");
+            if(maxGoal >= 3)
+            {
+                Console.WriteLine($"He has scored {maxGoal} goals and made a hat-trick !!!");
+            }
+            else
+            {
+                Console.WriteLine($"He has scored {maxGoal} goals.");
+            }
+                    
+        }
+    }
+}
