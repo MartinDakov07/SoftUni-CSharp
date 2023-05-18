5bd873379279a4b7f1ae2d75c5e853a6733231c9
Martin Dakov <martindakov6@gmail.com>
2023-05-18 23:25:48 +0300

    add file

---
+++ C# Basics/Exams/Programming Basics Online Pre - Exam - 28 and 29 May 2022/02. Bracelet Stand/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace _02._Bracelet_Stand
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double moneyPerDay = double.Parse(Console.ReadLine());
+            double priceMoney = double.Parse(Console.ReadLine());
+            double razhods = double.Parse(Console.ReadLine());
+            double presentPrice = double.Parse(Console.ReadLine());
+            double moneyForPeriod = moneyPerDay * 5;
+            double wonMoney = priceMoney * 5;
+            double totalMoney = (moneyForPeriod + wonMoney)-razhods;
+          
+            if(totalMoney > presentPrice)
+            {
+                Console.WriteLine($"Profit: {totalMoney:f2} BGN, the gift has been purchased.");
+            }
+            else
+            {
+                Console.WriteLine($"Insufficient money: {Math.Abs(presentPrice-totalMoney):f2} BGN.");
+            }
+
+        }
+    }
+}
