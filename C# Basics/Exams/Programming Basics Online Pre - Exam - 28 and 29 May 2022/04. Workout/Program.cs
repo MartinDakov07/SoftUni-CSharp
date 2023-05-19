c864766ed7d95870b8c62ddfba72c7a376ad33bf
Martin Dakov <martindakov6@gmail.com>
2023-05-19 22:55:24 +0300

    add file

---
+++ C# Basics/Exams/Programming Basics Online Pre - Exam - 28 and 29 May 2022/04. Workout/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace _04._Workout
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int days = int.Parse(Console.ReadLine());
+            double kilometersForFirstDay = int.Parse(Console.ReadLine());
+            int percentPerDay = int.Parse(Console.ReadLine());
+            double totalKilometers = kilometersForFirstDay + (kilometersForFirstDay * percentPerDay)/100;
+            double totalSuperKilometers = kilometersForFirstDay + totalKilometers;
+            for(int i = 2; i <= days; i++)
+            {
+                 percentPerDay = int.Parse(Console.ReadLine());
+
+                totalKilometers = totalKilometers + (totalKilometers * percentPerDay) / 100;
+                totalSuperKilometers += totalKilometers;
+            }
+
+            if (totalSuperKilometers >= 1000)
+            {
+                Console.WriteLine($"You've done a great job running {Math.Ceiling(Math.Abs(1000 - totalSuperKilometers))} more kilometers!");
+
+            }
+            else if(totalSuperKilometers < 1000)
+            {
+                Console.WriteLine($"Sorry Mrs. Ivanova, you need to run {Math.Ceiling(Math.Abs(1000 - totalSuperKilometers))} more kilometers");
+
+            }
+        }
+    }
+}
