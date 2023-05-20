45d4f5e9754735d5d2bc9d3a59f757757da8543a
Martin Dakov <martindakov6@gmail.com>
2023-05-20 23:30:47 +0300

    add file

---
+++ C# Basics/Exams/Programming Basics Online Regular Exam - 4 and 5 June 2022/02. Beer And Chips/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace _02._Beer_And_Chips
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string fan = Console.ReadLine();
+            double budget = double.Parse(Console.ReadLine());
+            int numOfBeer = int.Parse(Console.ReadLine());
+            int numOfChips = int.Parse(Console.ReadLine());
+            double priceForBeer = numOfBeer * 1.20;
+            double priceForPacketChips =priceForBeer * 0.45;
+            double totalPriceForChips = Math.Ceiling (numOfChips * priceForPacketChips);
+            double totalPrice = priceForBeer + totalPriceForChips;
+            double leftMoney = budget - totalPrice;
+            double neededMoney = totalPrice - budget;
+            if(budget >= totalPrice)
+            {
+                Console.WriteLine($"{fan} bought a snack and has {leftMoney:f2} leva left.");
+            }
+            else
+            {
+                Console.WriteLine($"{fan} needs {Math.Abs(neededMoney):f2} more leva!");
+            }
+
+        }
+    }
+}
