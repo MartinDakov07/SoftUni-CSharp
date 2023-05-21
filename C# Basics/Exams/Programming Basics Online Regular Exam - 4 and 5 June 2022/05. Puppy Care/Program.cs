c937aea2c4f7f593bede095b34a8ab68f8ad794c
Martin Dakov <martindakov6@gmail.com>
2023-05-21 23:25:05 +0300

    add file

---
+++ C# Basics/Exams/Programming Basics Online Regular Exam - 4 and 5 June 2022/05. Puppy Care/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace _05._Puppy_Care
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int totalFoodBought = int.Parse(Console.ReadLine())*1000;
+            string grams = Console.ReadLine();
+            double gramsForDog = 0;
+            double totalGrams = 0;
+            while (grams != "Adopted") 
+            {
+                gramsForDog = double.Parse(grams);
+                totalGrams += gramsForDog;
+                
+                
+                    
+                
+                grams = Console.ReadLine();
+
+            }
+            if (totalFoodBought >= totalGrams)
+            {
+                Console.WriteLine($"Food is enough! Leftovers: {totalFoodBought - totalGrams} grams.");
+               
+            }
+            else
+            {
+                Console.WriteLine($"Food is not enough. You need {Math.Abs(totalGrams - totalFoodBought)} grams more.");
+                
+            }
+        }
+    }
+}
