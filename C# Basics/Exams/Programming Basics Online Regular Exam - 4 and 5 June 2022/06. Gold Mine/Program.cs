1f44cff8282715ddd019068a005fd6bb34656018
Martin Dakov <martindakov6@gmail.com>
2023-05-22 22:50:38 +0300

    add file

---
+++ C# Basics/Exams/Programming Basics Online Regular Exam - 4 and 5 June 2022/06. Gold Mine/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace _06._Gold_Mine
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numOfLocations = int.Parse(Console.ReadLine());
+            double totalKilograms = 0;
+            double kilograms = 0;
+            
+            double averageNeedGold = 0;
+            int days = 0;
+            for (int i = 0; i < numOfLocations; i++)
+            {
+                
+                totalKilograms = 0;
+                 averageNeedGold = double.Parse(Console.ReadLine());
+                 days = int.Parse(Console.ReadLine());
+                for (int j = 0; j < days; j++)
+                {
+                    kilograms = double.Parse(Console.ReadLine());
+                    totalKilograms += kilograms;                
+                }
+                totalKilograms = totalKilograms / days;
+                if (totalKilograms >= averageNeedGold)
+                {
+                    Console.WriteLine($"Good job! Average gold per day: {totalKilograms:f2}.");
+                    continue;
+                }
+                else
+                {
+                    Console.WriteLine($"You need {(averageNeedGold - totalKilograms):f2} gold.");
+                    continue;
+                }
+            }
+            
+        }
+    }
+}
