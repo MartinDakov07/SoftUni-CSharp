ebe5aaba348947b935fefa4de1a1774991b5bea7
Martin Dakov <martindakov6@gmail.com>
2023-05-24 23:07:33 +0300

    add file

---
+++ C# Fundamentals/My Exams/Programming Fundamentals Mid Exam - 23 October 2022/03. Chat Logger/Program.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+namespace _03._Chat_Logger
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> chat = new List<string>();
+            string command = Console.ReadLine();
+            while(command != "end")
+            {
+               List <string> commands = command.Split(" ").ToList();
+                string commandArg = commands[0];
+                string messages = commands[1];
+                if(commandArg == "Chat")
+                {
+                    chat.Add(messages);
+                }
+                else if(commandArg == "Delete")
+                {
+                    if (chat.Contains(messages))
+                    {
+                        chat.Remove(messages);
+                    }
+                    else
+                    {
+                        command = Console.ReadLine();
+                        continue;
+                    }
+                }
+                else if(commandArg == "Edit")
+                {
+                    string mesForEdit = commands[2];
+                    if (chat.Contains(messages))
+                    {
+                        for(int i = 0; i < chat.Count; i++)
+                        {
+                            if(chat[i] == messages)
+                            {
+                                chat.RemoveAt(i);
+                                chat.Insert(i, mesForEdit);
+                                break;
+                            }
+                            
+                        }
+
+                    }
+                    else
+                    {
+                        command = Console.ReadLine();
+                        continue;
+                    }
+                }
+                else if(commandArg == "Pin")
+                {
+                    string messageCopy = messages;
+                    if (chat.Contains(messages))
+                    {
+                        for(int i = 0; i < chat.Count; i++)
+                        {
+                            if(chat[i] == messages)
+                            {
+                                chat.RemoveAt(i);
+                                chat.Add(messageCopy);
+                                break;
+                            }
+                        }
+                    }
+                    else
+                    {
+                        command = Console.ReadLine();
+                        continue;
+                    }
+                }
+                else if(commandArg == "Spam")
+                {
+                    for(int i = 1; i < commands.Count; i++)
+                    {
+                        chat.Add(commands[i]);
+                    }
+                }
+                command = Console.ReadLine();
+            }
+           for(int i = 0; i < chat.Count; i++)
+            {
+                Console.WriteLine(chat[i]);
+            }
+        }
+    }
+}
