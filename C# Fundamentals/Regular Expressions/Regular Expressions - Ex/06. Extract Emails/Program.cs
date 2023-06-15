e69ec1872695cf5d4b1917ba3150241d2d32b0c6
Martin Dakov <martindakov6@gmail.com>
2023-06-15 23:07:49 +0300

    add final file
    This course though me a lot of things

---
+++ C# Fundamentals/Regular Expressions/Regular Expressions - Ex/06. Extract Emails/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Numerics;
+using System.Text.RegularExpressions;
+namespace _06._Extract_Emails
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            string pattern = @"\s\b[a-zA-Z\d]+([-._][a-zA-Z\d]+)*[@][a-zA-z]+([-][a-zA-z]+)*([.]([a-zA-z]+([-][a-zA-z]+)*))+";
+            MatchCollection matches = Regex.Matches(input, pattern);
+            Console.WriteLine(String.Join("\n", matches));
+        }
+    }
+}
