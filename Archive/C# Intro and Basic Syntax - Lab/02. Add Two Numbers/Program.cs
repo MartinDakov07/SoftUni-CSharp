6b105df38de021526264bd6965d37b1e0ebc4374
Martin Dakov <martindakov6@gmail.com>
2023-06-18 22:47:44 +0300

    add file

---
+++ Archive/C# Intro and Basic Syntax - Lab/02. Add Two Numbers/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace _02._Add_Two_Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = int.Parse(Console.ReadLine());
+            int y = int.Parse(Console.ReadLine());
+            Console.WriteLine($"{x} + {y} = {x+y}");
+        }
+    }
+}
