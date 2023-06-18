4aac0a444884d6a63be8859957490590ab56ab9b
Martin Dakov <martindakov6@gmail.com>
2023-06-18 22:47:34 +0300

    add file

---
+++ Archive/C# Intro and Basic Syntax - Lab/01.Greeting/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace C__Intro_and_Basic_Syntax___Lab
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = Console.ReadLine();
+            Console.WriteLine($"Hello, {name}!");
+        }
+    }
+}
