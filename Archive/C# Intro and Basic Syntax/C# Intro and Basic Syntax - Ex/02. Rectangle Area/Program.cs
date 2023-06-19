8186c422ffea6a3156b1bf362cb9fef4d71bb637
Martin Dakov <martindakov6@gmail.com>
2023-06-19 22:49:38 +0300

    add file

---
+++ Archive/C# Intro and Basic Syntax/C# Intro and Basic Syntax - Ex/02. Rectangle Area/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace _02._Rectangle_Area
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double num1 = double.Parse(Console.ReadLine());
+            double num2 = double.Parse(Console.ReadLine());
+            Console.WriteLine($"{(num1*num2):f2}");
+        }
+    }
+}
