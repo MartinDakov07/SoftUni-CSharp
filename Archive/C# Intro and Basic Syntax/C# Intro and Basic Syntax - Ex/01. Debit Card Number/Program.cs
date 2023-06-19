d055c05b95297b25f81502063050d2c7f44a1895
Martin Dakov <martindakov6@gmail.com>
2023-06-19 22:49:24 +0300

    add file

---
+++ Archive/C# Intro and Basic Syntax/C# Intro and Basic Syntax - Ex/01. Debit Card Number/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace _01._Debit_Card_Number
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = int.Parse(Console.ReadLine());
+            int num2 = int.Parse(Console.ReadLine());
+            int num3 = int.Parse(Console.ReadLine());
+            int num4 = int.Parse(Console.ReadLine());
+            Console.Write($"{num1:D4} ");
+            Console.Write($"{num2:D4} ");
+            Console.Write($"{num3:D4} ");
+            Console.Write($"{num4:D4} ");
+        }
+    }
+}
