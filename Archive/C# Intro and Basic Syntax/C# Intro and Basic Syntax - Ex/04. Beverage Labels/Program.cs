e3d4b47ce6b7d136f6b3367ec2c833f4bd73e6ba
Martin Dakov <martindakov6@gmail.com>
2023-06-20 22:17:24 +0300

    add file

---
+++ Archive/C# Intro and Basic Syntax/C# Intro and Basic Syntax - Ex/04. Beverage Labels/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace _04._Beverage_Labels
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = Console.ReadLine();
+            double volume = double.Parse(Console.ReadLine());
+            double energy = double.Parse(Console.ReadLine());
+            double sugar = double.Parse(Console.ReadLine());
+
+            double quantity = volume / 100;
+
+            energy = energy * quantity;
+            sugar = sugar * quantity;
+
+            Console.WriteLine($"{volume}ml {name}:");
+            Console.WriteLine($"{energy:f2}kcal, {sugar:f2}g sugars");
+        }
+    }
+}
