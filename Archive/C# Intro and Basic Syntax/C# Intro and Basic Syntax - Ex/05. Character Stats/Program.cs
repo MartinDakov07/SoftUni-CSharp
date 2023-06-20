4fae28801615882c17303bdfe626acacb16f1656
Martin Dakov <martindakov6@gmail.com>
2023-06-20 22:17:42 +0300

    add file

---
+++ Archive/C# Intro and Basic Syntax/C# Intro and Basic Syntax - Ex/05. Character Stats/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace _05._Character_Stats
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = Console.ReadLine();
+            int currHealth = int.Parse(Console.ReadLine());
+            int maxHealth = int.Parse(Console.ReadLine());
+            int currEnergy = int.Parse(Console.ReadLine());
+            int maxEnergy = int.Parse(Console.ReadLine());
+
+            int leftHealth = maxHealth - currHealth;
+            int leftEnergy = maxEnergy - currEnergy;
+
+            Console.WriteLine($"Name: {name}");
+            Console.Write($"Health: |");
+            for(int i = 0; i< currHealth; i++)
+            {
+                Console.Write("|");
+            }
+            for(int i = 0; i < leftHealth; i++)
+            {
+                Console.Write(".");
+            }
+            Console.Write("|");
+
+            Console.WriteLine();
+
+            Console.Write($"Energy: |");
+            for (int i = 0; i < currEnergy; i++)
+            {
+                Console.Write("|");
+            }
+            for (int i = 0; i < leftEnergy; i++)
+            {
+                Console.Write(".");
+            }
+            Console.Write("|");
+        }
+    }
+}
