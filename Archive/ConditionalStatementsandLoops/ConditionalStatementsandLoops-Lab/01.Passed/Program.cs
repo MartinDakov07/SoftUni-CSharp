b3493e6abfaf8a73f7bed3922a255d46bd8428b6
Martin Dakov <martindakov6@gmail.com>
2023-06-21 22:28:55 +0300

    add file

---
+++ Archive/ConditionalStatementsandLoops/ConditionalStatementsandLoops-Lab/01.Passed/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace C__Conditional_Statements_and_Loops___Lab
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double grade = double.Parse(Console.ReadLine());
+            if(grade >= 3.00)
+            {
+                Console.WriteLine("Passed!");
+            }
+        }
+    }
+}
