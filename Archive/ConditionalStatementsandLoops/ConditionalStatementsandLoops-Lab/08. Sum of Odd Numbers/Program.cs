095c85616a971d0039135aeec898acbe86fec2ca
Martin Dakov <martindakov6@gmail.com>
2023-06-23 23:20:16 +0300

    add file

---
+++ Archive/ConditionalStatementsandLoops/ConditionalStatementsandLoops-Lab/08. Sum of Odd Numbers/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _08._Sum_of_Odd_Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int sum = 0;
+            int oddNum = 1;
+            for(int i = 1; i <= n; i++)
+            {
+                Console.WriteLine(oddNum);
+                sum += oddNum;                
+                oddNum += 2;
+            }
+            Console.WriteLine($"Sum: {sum}");
+        }
+    }
+}
