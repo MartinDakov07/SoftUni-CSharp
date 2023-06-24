160596d5135e1fe01625e6fa693b40f238c316e6
Martin Dakov <martindakov6@gmail.com>
2023-06-24 22:58:48 +0300

    add file

---
+++ Archive/ConditionalStatementsandLoops/CondlStatementsLoops-Lab/12. Number Checker/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _12._Number_Checker
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            try
+            {
+                int number = int.Parse(input);
+                Console.WriteLine("It is a number.");
+            }
+            catch
+            {
+                Console.WriteLine("Invalid input!");
+            }
+        }
+    }
+}
