4b00182df29af97cccad068e6414126723b3d25b
Martin Dakov <martindakov6@gmail.com>
2023-06-24 22:58:31 +0300

    add file

---
+++ Archive/ConditionalStatementsandLoops/CondlStatementsLoops-Lab/11. Odd Number/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace _11._Odd_Number
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+            while (true)
+            {
+                if(number % 2 == 0)
+                {
+                    Console.WriteLine("Please write an odd number.");
+                }
+                else
+                {
+                    Console.WriteLine($"The number is: {Math.Abs(number)}");
+                    return;
+                }
+                number = int.Parse(Console.ReadLine());
+            }
+        }
+    }
+}
