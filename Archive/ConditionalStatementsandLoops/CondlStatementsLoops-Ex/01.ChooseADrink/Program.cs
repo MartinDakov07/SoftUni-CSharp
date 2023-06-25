ba3d323ba9b0d61c96e331d2d57aa756a35f2ee8
Martin Dakov <martindakov6@gmail.com>
2023-06-25 22:51:42 +0300

    add file

---
+++ Archive/ConditionalStatementsandLoops/CondlStatementsLoops-Ex/01.ChooseADrink/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace C__Conditional_Statements_and_Loops___Exercises
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string profession = Console.ReadLine();
+            if(profession == "Athlete")
+            {
+                Console.WriteLine("Water");
+            }
+            else if(profession == "Businessman" || profession == "Businesswoman")
+            {
+                Console.WriteLine("Coffee");
+            }
+            else if(profession == "SoftUni Student")
+            {
+                Console.WriteLine("Beer");
+            }
+            else
+            {
+                Console.WriteLine("Tea");
+            }
+        }
+    }
+}
