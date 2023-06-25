e4ef25c6fb11d583fc6724917cd6f429d302b5f9
Martin Dakov <martindakov6@gmail.com>
2023-06-25 22:52:15 +0300

    add file

---
+++ Archive/ConditionalStatementsandLoops/CondlStatementsLoops-Ex/02. ChooseADrink2.0/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace _02._Choose_a_Drink_2._0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string profession = Console.ReadLine();
+            int quantity = int.Parse(Console.ReadLine());
+            double price = 0;
+            if (profession == "Athlete")
+            {
+                price = quantity * 0.7;
+            }
+            else if (profession == "Businessman" || profession == "Businesswoman")
+            {
+                price = quantity * 1;
+            }
+            else if (profession == "SoftUni Student")
+            {
+                price = quantity * 1.7;
+            }
+            else
+            {
+                price = quantity * 1.2;
+            }
+            Console.WriteLine($"The {profession} has to pay {price:f2}.");
+        }
+    }
+}
