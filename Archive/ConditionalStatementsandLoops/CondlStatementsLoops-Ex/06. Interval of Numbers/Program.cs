93f30f534d42a3e10da37dd2a932388ab0b23411
Martin Dakov <martindakov6@gmail.com>
2023-06-26 23:34:07 +0300

    add last for the day file

---
+++ Archive/ConditionalStatementsandLoops/CondlStatementsLoops-Ex/06. Interval of Numbers/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace _06._Interval_of_Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstNum = int.Parse(Console.ReadLine());
+            int secondNum = int.Parse(Console.ReadLine());
+            int biggerNum = firstNum < secondNum ? secondNum : firstNum;
+            int smallerNum = firstNum > secondNum ? secondNum : firstNum;
+            for(int i = smallerNum; i <= biggerNum; i++)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
