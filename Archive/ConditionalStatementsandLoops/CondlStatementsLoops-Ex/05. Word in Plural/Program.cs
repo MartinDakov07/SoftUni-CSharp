8aa6d556afdffa3c5fd78b28233261835be8a9f8
Martin Dakov <martindakov6@gmail.com>
2023-06-26 23:32:54 +0300

    add file

---
+++ Archive/ConditionalStatementsandLoops/CondlStatementsLoops-Ex/05. Word in Plural/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace _05._Word_in_Plural
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string word = Console.ReadLine();
+            if (word.EndsWith("y"))
+            {
+                word = word.Remove(word.Length - 1);
+                word = word.Insert(word.Length, "ies");
+            }
+            else if(word.EndsWith("o") || word.EndsWith("ch") || word.EndsWith("s") || word.EndsWith("sh") 
+                || word.EndsWith("x") || word.EndsWith("z"))
+            {                
+                word = word.Insert(word.Length, "es");
+            }
+            else
+            {              
+                word = word.Insert(word.Length, "s");
+            }
+            Console.WriteLine(word);
+        }
+    }
+}
