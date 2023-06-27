0d096afc6ac50ec2df344ff41053cf87e76b0413
Martin Dakov <martindakov6@gmail.com>
2023-06-27 22:50:28 +0300

    add file

---
+++ Archive/ConditionalStatementsandLoops/CondlStatementsLoops-Ex/09. Count the Integers/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace _09._Count_the_Integers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string num = Console.ReadLine();
+            int count = 0;
+            while (true)
+            {
+                try
+                {
+                    int number = int.Parse(num);
+                    count++;
+                }
+                catch
+                {
+                    break;
+                }
+                num = Console.ReadLine();
+            }
+            Console.WriteLine(count);
+        }
+    }
+}
