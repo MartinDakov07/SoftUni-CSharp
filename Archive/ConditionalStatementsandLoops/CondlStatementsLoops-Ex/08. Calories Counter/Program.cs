af3c766c2d8ca7a9ed8534ab6e06785eb23b7621
Martin Dakov <martindakov6@gmail.com>
2023-06-27 22:50:17 +0300

    add file

---
+++ Archive/ConditionalStatementsandLoops/CondlStatementsLoops-Ex/08. Calories Counter/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace _08._Calories_Counter
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int totalCalories = 0;
+            for(int i = 0; i < n; i++)
+            {
+                string product = Console.ReadLine().ToLower();
+                if(product == "cheese")
+                {
+                    totalCalories += 500;
+                }
+                else if(product == "tomato sauce")
+                {
+                    totalCalories += 150;
+                }
+                else if(product == "salami")
+                {
+                    totalCalories += 600;
+                }
+                else if(product == "pepper")
+                {
+                    totalCalories += 50;
+                }
+            }
+            Console.WriteLine($"Total calories: {totalCalories}");
+        }
+    }
+}
