af56bb2a26288e4c1e23af3515ecb64aae8def08
Martin Dakov <martindakov6@gmail.com>
2023-06-27 22:50:08 +0300

    add file

---
+++ Archive/ConditionalStatementsandLoops/CondlStatementsLoops-Ex/07. Cake Ingredients/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace _07._Cake_Ingredients
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string product;
+            int count = 0;
+            while((product = Console.ReadLine()) != "Bake!")
+            {
+                Console.WriteLine($"Adding ingredient {product}.");
+                count++;
+            }
+            Console.WriteLine($"Preparing cake with {count} ingredients.");
+        }
+    }
+}
