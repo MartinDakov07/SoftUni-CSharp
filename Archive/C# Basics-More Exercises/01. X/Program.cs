31c1efc5084f726a425467c8f87052b0a2db6956
Martin Dakov <martindakov6@gmail.com>
2023-06-30 23:11:43 +0300

    add file

---
+++ Archive/C# Basics-More Exercises/01. X/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace _01._X
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    if (j == i || j == n - i -1)
+                        Console.Write("x");
+                    else
+                        Console.Write(" ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
