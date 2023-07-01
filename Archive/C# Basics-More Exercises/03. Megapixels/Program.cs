7b968ecf2355b5dac3f777b0b8c794927d6b708a
Martin Dakov <martindakov6@gmail.com>
2023-07-01 23:05:06 +0300

    add file

---
+++ Archive/C# Basics-More Exercises/03. Megapixels/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace _03._Megapixels
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            decimal width = int.Parse(Console.ReadLine());
+            decimal height = decimal.Parse(Console.ReadLine());
+            decimal mp = Math.Round((width * height / 1_000_000), 1);                         
+            Console.WriteLine($"{width}x{height} => {mp}MP");                                                             
+        }            
+    }
+}
