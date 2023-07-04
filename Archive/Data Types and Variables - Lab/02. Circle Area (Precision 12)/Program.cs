cb54016e414efff258609621b802a2645651ce01
Martin Dakov <martindakov6@gmail.com>
2023-07-04 23:19:07 +0300

    add file

---
+++ Archive/Data Types and Variables - Lab/02. Circle Area (Precision 12)/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace _02._Circle_Area__Precision_12_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double r = double.Parse(Console.ReadLine());
+            Console.WriteLine($"{(Math.PI * r * r):f12}");
+        }
+    }
+}
