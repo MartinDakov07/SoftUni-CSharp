bd30117ef84170c1dcc90460f3bb90a9560d19ad
Martin Dakov <martindakov6@gmail.com>
2023-07-06 23:12:25 +0300

    add file

---
+++ Archive/Data Types and Variables/Data Types and Variables - Lab/07. Greeting/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using System.Xml.Linq;
+
+namespace _07._Greeting
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string firstName = Console.ReadLine();
+            string lastName = Console.ReadLine();
+            int age = int.Parse(Console.ReadLine());
+            Console.WriteLine($"Hello, {firstName} {lastName}. You are {age} years old.");
+        }
+    }
+}
