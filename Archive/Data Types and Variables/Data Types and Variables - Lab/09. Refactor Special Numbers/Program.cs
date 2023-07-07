9e5ae0baf25ac125d1d1cb20b31a0217fbe0ed9c
Martin Dakov <martindakov6@gmail.com>
2023-07-07 22:53:49 +0300

    add file
    Data Types are easy, dw

---
+++ Archive/Data Types and Variables/Data Types and Variables - Lab/09. Refactor Special Numbers/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace _09._Refactor_Special_Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int sum = 0; 
+            int copyNum = 0;
+            bool isTrue = false;
+            for (int i = 1; i <= n; i++)
+            {
+                copyNum = i;
+                sum = 0;
+                while (copyNum > 0)
+                {
+                    sum += copyNum % 10;
+                    copyNum = copyNum / 10;
+                }
+                
+                isTrue = (sum == 5) || (sum == 7) || (sum == 11);               
+                Console.WriteLine($"{i} -> {isTrue}");                                
+            }
+
+        }
+    }
+}
