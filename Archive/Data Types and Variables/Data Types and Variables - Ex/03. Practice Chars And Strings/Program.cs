eb2da3230afae2307d637d1969ade7e98467510d
Martin Dakov <martindakov6@gmail.com>
2023-07-08 23:08:23 +0300

    add file

---
+++ Archive/Data Types and Variables/Data Types and Variables - Ex/03. Practice Chars And Strings/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _03._Practice_Chars_And_Strings
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = "Software University";
+            char b = 'B';
+            char y = 'y';
+            char e = 'e';
+            string fact = "I love programming";
+            Console.WriteLine(text);
+            Console.WriteLine(b);
+            Console.WriteLine(y);
+            Console.WriteLine(e);
+            Console.WriteLine(fact);
+        }
+    }
+}
