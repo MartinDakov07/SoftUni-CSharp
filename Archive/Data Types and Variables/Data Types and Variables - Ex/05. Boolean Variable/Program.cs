91f50adc46311ce9d8298ee66551b7bc03f79450
Martin Dakov <martindakov6@gmail.com>
2023-07-09 23:21:40 +0300

    add file

---
+++ Archive/Data Types and Variables/Data Types and Variables - Ex/05. Boolean Variable/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _05._Boolean_Variable
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string choice = Console.ReadLine();
+            bool convert = Convert.ToBoolean(choice);
+            if(convert == true)
+            {
+                Console.WriteLine("Yes");
+            }
+            else
+            {
+                Console.WriteLine("No");
+            }
+        }
+    }
+}
