0dd8be6610c314ded75d995ade724e8435b7d1f2
Martin Dakov <martindakov6@gmail.com>
2023-07-09 23:21:50 +0300

    add file

---
+++ Archive/Data Types and Variables/Data Types and Variables - Ex/06. Strings And Objects/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Linq;
+
+namespace _06._Strings_And_Objects
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string hello = "Hello ";
+            string world = "World";
+            string message = hello + world;
+            Console.WriteLine(message);
+        }
+    }
+}
