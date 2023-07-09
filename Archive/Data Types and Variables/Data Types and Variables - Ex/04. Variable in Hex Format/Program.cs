8fb0b1719e34c0ad6ad9b2075f2a5bbc46354710
Martin Dakov <martindakov6@gmail.com>
2023-07-09 23:21:30 +0300

    add file

---
+++ Archive/Data Types and Variables/Data Types and Variables - Ex/04. Variable in Hex Format/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace _04._Variable_in_Hex_Format
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string hexaFormat = Console.ReadLine();
+            decimal num = Convert.ToInt32(hexaFormat, 16);
+            Console.WriteLine(num);
+        }
+    }
+}
