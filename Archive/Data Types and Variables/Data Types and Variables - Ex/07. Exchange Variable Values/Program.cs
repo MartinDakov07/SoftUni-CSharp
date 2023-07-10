2affd04721225573bd5d2ff5008b39058a672cb4
Martin Dakov <martindakov6@gmail.com>
2023-07-10 22:53:47 +0300

    add new file

---
+++ Archive/Data Types and Variables/Data Types and Variables - Ex/07. Exchange Variable Values/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace _07._Exchange_Variable_Values
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstNum = int.Parse(Console.ReadLine());
+            int secondNum = int.Parse(Console.ReadLine());
+            Console.WriteLine("Before:");
+            Console.WriteLine($"a = {firstNum}");
+            Console.WriteLine($"b = {secondNum}");
+            Console.WriteLine("After:");
+            Console.WriteLine($"a = {secondNum}");
+            Console.WriteLine($"b = {firstNum}");
+        }
+    }
+}
