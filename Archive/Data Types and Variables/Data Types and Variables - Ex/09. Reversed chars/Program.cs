42e881a2258769d19aa7e40eb8ffededb5fda57a
Martin Dakov <martindakov6@gmail.com>
2023-07-11 22:56:51 +0300

    add file

---
+++ Archive/Data Types and Variables/Data Types and Variables - Ex/09. Reversed chars/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+
+namespace _09._Reversed_chars
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            char firstChar = char.Parse(Console.ReadLine());
+            char secondChar = char.Parse(Console.ReadLine());
+            char thirdChar = char.Parse(Console.ReadLine());
+            string text = firstChar.ToString() + secondChar.ToString() + thirdChar.ToString();
+            string newText = "";
+            //FirstCase
+            //text = text.Reverse().ToString();            
+            for(int i = text.Length - 1; i >= 0; i--)
+            {
+                 newText += text[i].ToString();
+            }
+            //ThirdCase- with foreach
+            Console.WriteLine(newText);
+        }
+    }
+}
