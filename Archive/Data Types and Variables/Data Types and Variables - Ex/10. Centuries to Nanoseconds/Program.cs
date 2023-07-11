e7737ce160438d46d3c5c872ab0bc59271d43f5c
Martin Dakov <martindakov6@gmail.com>
2023-07-11 22:57:00 +0300

    add file

---
+++ Archive/Data Types and Variables/Data Types and Variables - Ex/10. Centuries to Nanoseconds/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+using System.Numerics;
+
+namespace _10._Centuries_to_Nanoseconds
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int centuries = int.Parse(Console.ReadLine());
+            long years = centuries * 100;
+            long days = (long)(years * 365.2422);
+            BigInteger hours = days * 24;
+            BigInteger minutes = hours * 60;
+            BigInteger seconds = minutes * 60;
+            BigInteger millieseconds = seconds * 1000;
+            BigInteger microseconds = millieseconds * 1000;
+            BigInteger nanosecond = microseconds * 1000;
+            Console.WriteLine($"{centuries} centuries = {years} years = {days} days = {hours} hours = {minutes} minutes = {seconds} seconds = {millieseconds} milliseconds = {microseconds} microseconds = {nanosecond} nanoseconds");
+        }
+    }
+}
