17218f632147edf1ed0878859d6d3ed72b83b3a5
Martin Dakov <martindakov6@gmail.com>
2023-07-12 22:38:16 +0300

    add file

---
+++ Archive/Data Types and Variables/Data Types and Variables - Ex/12. Rectangle Properties/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _12._Rectangle_Properties
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double width = double.Parse(Console.ReadLine());
+            double height = double.Parse(Console.ReadLine());
+            double perimeter = width * 2 + height * 2;
+            double area = width * height;
+            double doubleWidth = Math.Pow(width, 2);
+            double doubleHeight = Math.Pow(height, 2);
+            double diagonal = Math.Sqrt(doubleWidth + doubleHeight);           
+            Console.WriteLine($"{perimeter}");
+            Console.WriteLine($"{area}");
+            Console.WriteLine($"{diagonal}");
+        }
+    }
+}
