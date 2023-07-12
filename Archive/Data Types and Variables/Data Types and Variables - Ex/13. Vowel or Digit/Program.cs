7852dd82f875dc7bdaeec4f5e7039b88042916b1
Martin Dakov <martindakov6@gmail.com>
2023-07-12 22:38:30 +0300

    add file

---
+++ Archive/Data Types and Variables/Data Types and Variables - Ex/13. Vowel or Digit/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace _13._Vowel_or_Digit
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string symbol = Console.ReadLine().ToLower();
+            try
+            {
+                int digit = int.Parse(symbol);
+                Console.WriteLine("digit");
+                return;
+            }
+            catch
+            {
+                if(symbol == "a" || symbol == "e" || symbol == "i" || symbol == "o" || symbol == "u" || symbol == "y")
+                {
+                    Console.WriteLine("vowel");
+                }
+                else
+                {
+                    Console.WriteLine("other");
+                }                
+                return;
+            }
+            
+        }
+    }
+}
