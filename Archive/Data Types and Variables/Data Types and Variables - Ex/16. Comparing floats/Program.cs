71feefe3102b45ca490c1e59d7bebceb3a9761db
Martin Dakov <martindakov6@gmail.com>
2023-07-13 22:56:11 +0300

    add file

---
+++ Archive/Data Types and Variables/Data Types and Variables - Ex/16. Comparing floats/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace _16._Comparing_floats
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            decimal firstNum = decimal.Parse(Console.ReadLine());
+            decimal secondNum = decimal.Parse(Console.ReadLine());
+            decimal bigger = firstNum > secondNum ? firstNum : secondNum;
+            decimal smaller = firstNum < secondNum ? firstNum : secondNum;
+            if(bigger - smaller < 0.000001m)
+            {
+                Console.WriteLine("True");
+            }
+            else
+            {
+                Console.WriteLine("False");
+            }
+        }
+    }
+}
