113bb03ca96d18c88b659a969a636764442701fa
Martin Dakov <martindakov6@gmail.com>
2023-07-16 22:54:58 +0300

    add file

---
+++ Archive/Data Types and Variables/Data Types and Variables - ME/05. Weather Forecast/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace _05._Weather_Forecast
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string number = Console.ReadLine();
+
+            try
+            {
+                sbyte sunny = sbyte.Parse(number);
+                Console.WriteLine("Sunny");
+                return;
+            }
+            catch
+            {
+
+            }
+
+            try
+            {
+                int cloudy = int.Parse(number);
+                Console.WriteLine("Cloudy");
+                return;
+            }
+            catch
+            {
+
+            }
+
+            try
+            {
+                long windy = long.Parse(number);
+                Console.WriteLine("Windy");
+                return;
+            }
+            catch
+            {
+
+            }
+            if (double.Parse(number) % 1 != 0)
+            {
+                Console.WriteLine("Rainy");
+                return;
+            }
+        }
+    }
+}
