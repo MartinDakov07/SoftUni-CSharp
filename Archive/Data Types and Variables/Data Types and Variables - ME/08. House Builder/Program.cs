8bd9fc6bad73550ac43c1ec390bfa4b5cc02c664
Martin Dakov <martindakov6@gmail.com>
2023-07-17 22:46:43 +0300

    add file

---
+++ Archive/Data Types and Variables/Data Types and Variables - ME/08. House Builder/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Numerics;//This will activate BigInteger variable
+namespace _08._House_Builder
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string num1 = Console.ReadLine();
+            string num2 = Console.ReadLine();
+            sbyte byteNum = 0;
+            BigInteger intNum = 0;
+            try
+            {
+                byteNum = sbyte.Parse(num1);
+            }
+            catch
+            {
+                intNum = BigInteger.Parse(num1);
+            }
+            try
+            {
+                byteNum = sbyte.Parse(num2);
+            }
+            catch
+            {
+                intNum = BigInteger.Parse(num2);
+            }
+            
+            BigInteger totalCost = (4 * byteNum) + (10 * intNum);
+            Console.WriteLine(totalCost);
+        }
+    }
+}
