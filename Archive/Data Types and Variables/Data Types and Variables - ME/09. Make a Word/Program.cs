eb4dd25c31de35f79de3441839fb4198bb779657
Martin Dakov <martindakov6@gmail.com>
2023-07-18 23:07:02 +0300

    add file

---
+++ Archive/Data Types and Variables/Data Types and Variables - ME/09. Make a Word/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+
+namespace _09._Make_a_Word
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+            string message = "";
+            for(int i = 0;i < num; i++)
+            {
+                char symbol = char.Parse(Console.ReadLine());
+                message += symbol.ToString();
+            }
+            Console.WriteLine($"The word is: {message}");
+        }
+    }
+}
