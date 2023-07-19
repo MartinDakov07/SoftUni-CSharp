b57536cb50bfd308d115b9c1539707309d34c656
Martin Dakov <martindakov6@gmail.com>
2023-07-19 23:19:59 +0300

    add file

---
+++ Archive/Data Types and Variables/Data Types and Variables - ME/13. Decrypting Message/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _13._Decrypting_Message
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int key = int.Parse(Console.ReadLine());
+            int n = int.Parse(Console.ReadLine());
+            string message = "";
+            for (int i = 0; i < n; i++)
+            {
+                char symbol = char.Parse(Console.ReadLine());
+                message += (char)((int)symbol + key);
+            }
+            Console.WriteLine(message);
+        }
+    }
+}
