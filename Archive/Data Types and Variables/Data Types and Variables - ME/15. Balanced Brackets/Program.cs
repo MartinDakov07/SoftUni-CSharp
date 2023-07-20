78503a40cc1631bd683a4813f016037d61ab2a23
Martin Dakov <martindakov6@gmail.com>
2023-07-20 22:58:27 +0300

    add file

---
+++ Archive/Data Types and Variables/Data Types and Variables - ME/15. Balanced Brackets/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace _15._Balanced_Brackets
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            bool isBalanced = true;
+            bool openedBracket = false;
+            for(int i = 0; i < n; i++)
+            {
+                string bracket = Console.ReadLine();
+                if(bracket == "(")
+                {
+                    if (openedBracket)
+                    {
+                        isBalanced = false;
+                    }
+                    else
+                    {
+                        openedBracket = true;
+                    }
+                }
+                else if(bracket == ")")
+                {
+
+                    if (!openedBracket)
+                    {
+                        isBalanced = false;
+                    }
+                    else
+                    {
+                        openedBracket = false;
+                    }                    
+                }
+            }
+            if (openedBracket)
+            {
+                isBalanced = false;
+            }
+            if (isBalanced)
+            {
+                Console.WriteLine("BALANCED");
+            }
+            else
+            {
+                Console.WriteLine("UNBALANCED");
+            }
+        }
+    }
+}
