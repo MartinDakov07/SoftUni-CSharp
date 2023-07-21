2de9550142331253977f1007e2338641f8adde4b
Martin Dakov <martindakov6@gmail.com>
2023-07-21 22:46:30 +0300

    add file

---
+++ Archive/Methods/Methods. Debugging and Troubleshooting Code - Lab/01. Blank Receipt/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace _01._Blank_Receipt
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            PrintReceipt();
+        }
+        static void PrintHeader()
+        {
+            Console.WriteLine("CASH RECEIPT");
+            Console.WriteLine("------------------------------");
+        }
+        static void PrintBody()
+        {
+            Console.WriteLine("Charged to____________________");
+            Console.WriteLine("Received by___________________");
+        }
+        static void PrintFooter()
+        {
+            Console.WriteLine("------------------------------");
+            Console.WriteLine("\u00a9 SoftUni");
+        }
+        static void PrintReceipt()
+        {
+            PrintHeader();
+            PrintBody();
+            PrintFooter();
+        }
+    }
+}
