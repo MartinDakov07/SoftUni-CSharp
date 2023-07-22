7401635de5c4f4e913f183e9af6f9acb826469cc
Martin Dakov <martindakov6@gmail.com>
2023-07-22 23:15:24 +0300

    add file

---
+++ Archive/Methods/Methods. Debugging and Troubleshooting Code - Lab/03. Printing Triangle/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace _03._Printing_Triangle
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            for(int i = 0; i < n; i++)
+            {
+                PrintLine(1, i);
+            }
+            PrintLine(1, n);
+            for(int i = n-1;i >= 0; i--)
+            {
+                PrintLine(1, i);
+            }
+        }
+        static void PrintLine(int start, int end)
+        {
+            for(int i = start; i <= end; i++)
+            {
+                Console.Write(i + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
