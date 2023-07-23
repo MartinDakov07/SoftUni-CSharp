c9dcb63bfe5e258eca790dcac89372b2b9dffb4f
Martin Dakov <martindakov6@gmail.com>
2023-07-23 22:54:12 +0300

    add file

---
+++ Archive/Methods/Methods. Debugging and Troubleshooting Code - Lab/05. Temperature Conversion/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace _05._Temperature_Conversion
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double farenhait = double.Parse(Console.ReadLine());
+            double celsius = FarenhaitToCelsius(farenhait);
+            Console.WriteLine($"{celsius:f2}");
+        }
+        static double FarenhaitToCelsius(double farenhait)
+        {
+            return (farenhait - 32) * 5 / 9;
+        }
+    }
+}
