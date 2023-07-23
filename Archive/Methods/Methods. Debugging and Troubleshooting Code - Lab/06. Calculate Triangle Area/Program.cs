a195891f0a48062926025e0e985d50af28f4d6e7
Martin Dakov <martindakov6@gmail.com>
2023-07-23 22:54:21 +0300

    add file

---
+++ Archive/Methods/Methods. Debugging and Troubleshooting Code - Lab/06. Calculate Triangle Area/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace _06._Calculate_Triangle_Area
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double width = double.Parse(Console.ReadLine());
+            double height = double.Parse(Console.ReadLine());
+            double area = FindTriangleArea(width, height);
+            Console.WriteLine($"{area}");
+        }
+        static double FindTriangleArea(double width, double height)
+        {
+            return width * height / 2;
+        }
+    }
+}
