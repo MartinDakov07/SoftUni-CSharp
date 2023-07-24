15ea5d12c9b76df052442475a4cb9e2f935d2772
Martin Dakov <martindakov6@gmail.com>
2023-07-24 23:13:49 +0300

    add file

---
+++ Archive/Methods/Methods. Debugging and Troubleshooting Code - Lab/07. Math Power/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace _07._Math_Power
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double num = double.Parse(Console.ReadLine());
+            double power = double.Parse(Console.ReadLine());
+            double result = FindPower(num, power);
+            Console.WriteLine(result);
+        }
+        static double FindPower(double n, double power)
+        {            
+            return Math.Pow(n, power);
+        }
+    }
+}
