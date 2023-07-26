d5cbcde2a5f5cec9aae56d7b99634cc8471875e2
Martin Dakov <martindakov6@gmail.com>
2023-07-26 22:16:13 +0300

    add file

---
+++ Archive/Methods/Methods. Debugging and Troubleshooting Code - Lab/10. HolidaysBetweenTwoDates/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Globalization;
+
+namespace _10._Holidays_Between_Two_Dates
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var startDate = DateTime.ParseExact(Console.ReadLine(),
+            "d.M.yyyy", CultureInfo.InvariantCulture);//if its dd and we take 1-9, it will write us with 0 in front
+            var endDate = DateTime.ParseExact(Console.ReadLine(),
+                "d.M.yyyy", CultureInfo.InvariantCulture);// plus M is for month, and mm is for minutes
+            var holidaysCount = 0;
+            for (var date = startDate; date <= endDate; date = date.AddDays(1))//date = date.AddDays(1) because it wont make anything
+                if (date.DayOfWeek == DayOfWeek.Saturday ||
+                    date.DayOfWeek == DayOfWeek.Sunday) holidaysCount++;// || instead of &&
+
+            Console.WriteLine(holidaysCount);
+
+        }
+    }
+}
