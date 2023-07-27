681c3d487baa7f540584841c0351ebde78d89bb4
Martin Dakov <martindakov6@gmail.com>
2023-07-27 22:52:41 +0300

    add file

---
+++ Archive/Methods/Methods. Debugging and Troubleshooting Code - Ex/02. Max Method/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _02._Max_Method
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            int biggestNum = GetMax(a, b);
+            Console.WriteLine(biggestNum);
+        }
+        static int GetMax(int a, int b)
+        {
+            int biggestNum = Math.Max(a, b);
+            int c = int.Parse(Console.ReadLine());
+            biggestNum = Math.Max(biggestNum, c);
+            return biggestNum;
+        }
+    }
+}
