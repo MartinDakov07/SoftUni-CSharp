4b2675ea6be8003039914b0dd856699987fd692a
Martin Dakov <martindakov6@gmail.com>
2023-07-27 22:52:31 +0300

    add file

---
+++ Archive/Methods/Methods. Debugging and Troubleshooting Code - Ex/01. Hello, Name!/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace _01._Hello__Name_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = Console.ReadLine();
+            PrintName(name);
+        }
+        static void PrintName(string name)
+        {
+            Console.WriteLine($"Hello, {name}!");
+        }
+    }
+}
