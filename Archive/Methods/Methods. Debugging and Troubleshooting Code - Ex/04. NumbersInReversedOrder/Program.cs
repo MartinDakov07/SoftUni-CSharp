cdf5f50666be62f4d97c88dbb8eed319f31a2183
Martin Dakov <martindakov6@gmail.com>
2023-07-28 23:01:07 +0300

    add file

---
+++ Archive/Methods/Methods. Debugging and Troubleshooting Code - Ex/04. NumbersInReversedOrder/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+
+namespace _04._Numbers_in_Reversed_Order
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string number = Console.ReadLine();
+            string reversedNum = ReversedNumber(number.ToString());
+            Console.WriteLine(reversedNum.ToString());
+        }
+        static string ReversedNumber(string number)
+        {
+            string reversedNumber = "";
+            for (int i = number.Length - 1; i >= 0; i--)
+            {
+                reversedNumber += number[i];
+            }
+            return reversedNumber;
+        }
+    }
+}
