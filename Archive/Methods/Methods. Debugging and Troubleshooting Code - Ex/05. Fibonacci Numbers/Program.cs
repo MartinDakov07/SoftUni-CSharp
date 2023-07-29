1ea7e5590ef5522ea2c4ecb8f249c363bc6e5b2b
Martin Dakov <martindakov6@gmail.com>
2023-07-29 22:43:01 +0300

    add file

---
+++ Archive/Methods/Methods. Debugging and Troubleshooting Code - Ex/05. Fibonacci Numbers/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace _05._Fibonacci_Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+            GetFibbonaciNum(number);
+        }
+        static void GetFibbonaciNum(int number)
+        {
+            int n1=0, n2=1, n3;
+            if(number == 0)
+            {
+                Console.WriteLine(1);
+                return;
+            }
+            for(int i = 1; i <= number; i++)
+            {
+                n3 = n1 + n2;
+                if(i == number)
+                {
+                    Console.Write($"{n3}");
+                }                
+                n1 = n2;
+                n2 = n3;
+            }
+        }
+    }
+}
