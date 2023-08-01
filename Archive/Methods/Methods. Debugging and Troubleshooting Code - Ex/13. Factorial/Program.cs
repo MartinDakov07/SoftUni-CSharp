8babc2edc9b5ebe7187267abe87e559cd2d9b27a
Martin Dakov <martindakov6@gmail.com>
2023-08-01 23:19:05 +0300

    add file

---
+++ Archive/Methods/Methods. Debugging and Troubleshooting Code - Ex/13. Factorial/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Numerics;
+namespace _13._Factorial
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            BigInteger sum = 1;
+            for(int i = 1; i <= n; i++)
+            {
+                sum *= i;
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
