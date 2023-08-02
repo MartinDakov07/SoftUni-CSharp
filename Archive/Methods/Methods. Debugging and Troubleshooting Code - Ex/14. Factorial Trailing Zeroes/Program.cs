a3b02cfd0dfe12f5beae9ebff8843dd2706ab1a3
Martin Dakov <martindakov6@gmail.com>
2023-08-02 23:16:41 +0300

    add file

---
+++ Archive/Methods/Methods. Debugging and Troubleshooting Code - Ex/14. Factorial Trailing Zeroes/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Drawing;
+using System.Numerics;
+namespace _14._Factorial_Trailing_Zeroes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            BigInteger sum = 1;
+            int count = 0;
+            for (int i = 1; i <= n; i++)
+            {
+                sum *= i;
+            }
+            ReturnTrailingZeroesCount(sum, count);
+        }
+        static void ReturnTrailingZeroesCount(BigInteger sum,int count)
+        {
+            while (sum > 0)
+            {
+                var rem = sum % 10;
+                if (rem == 0)
+                {
+                    count++;
+                }
+                else
+                {
+                    Console.WriteLine(count);
+                    return;
+                }
+                sum /= 10;
+            }
+        }
+    }
+}
