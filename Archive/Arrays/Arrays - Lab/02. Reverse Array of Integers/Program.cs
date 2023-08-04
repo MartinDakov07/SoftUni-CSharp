05d468614845cc0be290a8a1ec7199a4292d1551
Martin Dakov <martindakov6@gmail.com>
2023-08-04 23:00:35 +0300

    add file

---
+++ Archive/Arrays/Arrays - Lab/02. Reverse Array of Integers/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _02._Reverse_Array_of_Integers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int[] numbers = new int[n];
+            for(int i = n - 1; i >= 0; i--)
+            {
+                int num = int.Parse(Console.ReadLine());
+                numbers[i] = num;
+            }
+            for(int i = 0; i < numbers.Length; i++)
+            {
+                Console.Write(numbers[i] + " ");
+            }
+        }
+    }
+}
