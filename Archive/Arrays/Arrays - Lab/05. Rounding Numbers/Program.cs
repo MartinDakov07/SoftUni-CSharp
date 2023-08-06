720e01c42adf7a55517c758d9b37f2b6b24f2a83
Martin Dakov <martindakov6@gmail.com>
2023-08-06 23:14:35 +0300

    add file

---
+++ Archive/Arrays/Arrays - Lab/05. Rounding Numbers/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Numerics;
+namespace _05._Rounding_Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] numbers = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Select(double.Parse)
+                .ToArray();
+            int count = 0;
+            foreach(var num in numbers)
+            {
+                double number = num + 0.5;
+                if(number % 1 == 0.5)
+                {
+                    number = Math.Ceiling(Math.Abs(number));
+                }
+                numbers[count++] = Math.Abs(number);
+            }
+            Console.WriteLine(string.Join(" ",numbers));
+        }
+    }
+}
