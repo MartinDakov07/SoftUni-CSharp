6309f6c6e935383149f4ba689dce35c24db13bf9
Martin Dakov <martindakov6@gmail.com>
2023-08-06 23:14:56 +0300

    add other solution

---
+++ Archive/Arrays/Arrays - Lab/05.Rounding Numbers Away from Zero/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+
+namespace _05.Rounding_Numbers_Away_from_Zero
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] numbers = Console.ReadLine()
+                .Split()
+                .Select(double.Parse)
+                .ToArray();
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                Console.WriteLine($"{numbers[i]} => {(int)Math.Round(numbers[i], MidpointRounding.AwayFromZero)}");
+            }
+        }
+    }
+}
