bafc99fe21a6ef15d86115e9017a141c18219229
Martin Dakov <martindakov6@gmail.com>
2023-08-08 22:58:53 +0300

    add file

---
+++ Archive/Arrays/Arrays - Exercises/02. Rotate and Sum/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Numerics;
+namespace _02._Rotate_and_Sum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+            int k = int.Parse(Console.ReadLine());
+            int[] sum = new int[numbers.Length];
+            for(int i =0; i < k; i++)
+            {
+                int firstNum = numbers[numbers.Length - 1];
+                for(int j = numbers.Length-1;j > 0; j--)
+                {
+                    numbers[j] = numbers[j - 1];
+                }
+                numbers[0] = firstNum;
+                for(int l = 0; l < numbers.Length; l++)
+                {
+                    sum[l] += numbers[l];
+                }
+            }
+            Console.WriteLine(String.Join(" ",sum));
+        }
+    }
+}
