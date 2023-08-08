67b32dbe2bb54bd3798383d7b1e2875ca4686ec4
Martin Dakov <martindakov6@gmail.com>
2023-08-08 22:58:38 +0300

    add file

---
+++ Archive/Arrays/Arrays - Exercises/01. Largest Common End/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+namespace _01._Largest_Common_End
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] firstText = Console.ReadLine().Split();
+            string[] secondText = Console.ReadLine().Split();
+            int lowerLength = Math.Min(firstText.Length, secondText.Length);
+            int leftCorner = 0;
+            int rightCorner = 0;
+            for(int i = 0; i < lowerLength; i++)
+            {
+                if (firstText[i] == secondText[i])
+                {
+                    leftCorner++;
+                }
+                else
+                {
+                    break;
+                }                
+            }
+            Array.Reverse(firstText);
+            Array.Reverse(secondText);
+            for(int j = 0; j < lowerLength; j++)
+            {
+                if (firstText[j] == secondText[j])
+                {
+                    rightCorner++;
+                }
+                else
+                {
+                    break;
+                }
+            }
+            int longerLength = Math.Max(leftCorner, rightCorner);
+            Console.WriteLine(longerLength);
+        }        
+    }
+}
