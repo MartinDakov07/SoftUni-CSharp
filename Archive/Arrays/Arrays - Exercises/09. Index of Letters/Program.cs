6768d6f1c950470df3c1df7b511f18130916fbb3
Martin Dakov <martindakov6@gmail.com>
2023-08-12 22:40:48 +0300

    add file

---
+++ Archive/Arrays/Arrays - Exercises/09. Index of Letters/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace _09._Index_of_Letters
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            char[] allLetters = new char[]
+            {
+               'a','b','c',
+               'd','e','f',
+               'g','h','i',
+               'j','k','l',
+               'm','n','o',
+               'p','q','r',
+               's','t','u',
+               'v','w','x',
+               'y','z'
+            };
+            string message = Console.ReadLine();
+            for (int i = 0; i < message.Length; i++)
+            {
+                for (int j = 0; j < allLetters.Length; j++)
+                {
+                    if (message[i].ToString() == allLetters[j].ToString())
+                    {
+                        Console.WriteLine($"{message[i]} -> {j}");
+                        break;
+                    }
+                }
+            }
+        }
+    }
+}
