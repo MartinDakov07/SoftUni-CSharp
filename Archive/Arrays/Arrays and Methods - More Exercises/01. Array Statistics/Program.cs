3b294819eafa85fbe823a526157014e5aa2b6d54
Martin Dakov <martindakov6@gmail.com>
2023-08-18 22:55:21 +0300

    add file

---
+++ Archive/Arrays/Arrays and Methods - More Exercises/01. Array Statistics/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Numerics;
+using System.Threading.Tasks;
+namespace _01._Array_Statistics
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            long[] numbers = Console.ReadLine()
+                .Split()
+                .Select(long.Parse)
+                .ToArray();
+            long lowestNum = long.MaxValue;
+            long biggestNum = long.MinValue;
+            double sum = 0;
+            double average = 0;
+            for(int i = 0; i < numbers.Length; i++)
+            {
+                sum += numbers[i];
+
+                if (numbers[i] > biggestNum)
+                {
+                    biggestNum = numbers[i];
+                }
+                if (numbers[i] < lowestNum) 
+                {
+                    lowestNum = numbers[i];
+                }
+            }
+            average = sum / numbers.Length;
+            Console.WriteLine($"Min = {lowestNum}");
+            Console.WriteLine($"Max = {biggestNum}");
+            Console.WriteLine($"Sum = {sum}");
+            Console.WriteLine($"Average = {average}");
+        }
+    }
+}
