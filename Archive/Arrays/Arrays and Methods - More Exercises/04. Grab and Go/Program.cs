6b994d55c8233255ef8161219dc69b1c240c6f32
Martin Dakov <martindakov6@gmail.com>
2023-08-19 23:13:07 +0300

    add file

---
+++ Archive/Arrays/Arrays and Methods - More Exercises/04. Grab and Go/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Numerics;
+using System.Threading.Tasks;
+namespace _04._Grab_and_Go
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+            int num = int.Parse(Console.ReadLine());
+            bool isFound = false;
+            int sum = 0;
+            int index = 0;
+            for(int i = 0; i < numbers.Length; i++)
+            {
+                if (numbers[i] == num)
+                {
+                    index = i;
+                    isFound = true;
+                }
+            }
+            if (isFound)
+            {
+                for(int i = index;i >= 0; i--)
+                {
+                    sum += numbers[i];
+                }
+                Console.WriteLine(sum);
+            }
+            else
+            {
+                Console.WriteLine("No occurrences were found!");
+            }
+            
+        }
+    }
+}
