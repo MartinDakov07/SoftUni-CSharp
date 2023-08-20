970ebf44d053c6c08b2a6cb355b8168b66c3c11a
Martin Dakov <martindakov6@gmail.com>
2023-08-20 23:18:19 +0300

    add file

---
+++ Archive/Arrays/Arrays and Methods - More Exercises/05. Pizza Ingredients/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _05._Pizza_Ingredients
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] ingredients = Console.ReadLine().Split();
+            int length = int.Parse(Console.ReadLine());
+            int count = 0;
+            List<string> recipe = new List<string>();
+            for(int i = 0; i < ingredients.Length; i++)
+            {
+                if (ingredients[i].Length == length)
+                {
+                    if (count >= 10)
+                    {
+                        break;
+                    }
+                    Console.WriteLine($"Adding {ingredients[i]}.");
+                    recipe.Add(ingredients[i]);
+                    count++;
+                }               
+            }
+            Console.WriteLine($"Made pizza with total of {count} ingredients.");
+            Console.WriteLine($"The ingredients are: {string.Join(", ",recipe)}.");
+        }
+    }
+}
