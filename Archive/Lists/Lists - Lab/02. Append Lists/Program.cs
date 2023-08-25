c278cd79c9af130e76575c7cce7407fc9cb2f5eb
Martin Dakov <martindakov6@gmail.com>
2023-08-25 23:18:27 +0300

    add file

---
+++ Archive/Lists/Lists - Lab/02. Append Lists/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+using System.Numerics;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+namespace _02._Append_Lists
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            List<string> firstList = input
+                .Split('|', StringSplitOptions.RemoveEmptyEntries)
+                .Reverse()
+                .ToList();
+            List<int> secondList = new List<int>();
+            foreach (string numbers in firstList)
+            {
+                secondList.AddRange(numbers.Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToList());
+            }
+            Console.WriteLine(string.Join(" ", secondList));
+        }
+    }
+}
