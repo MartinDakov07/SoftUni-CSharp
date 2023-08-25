6f0083da6793f6ac8ae70af44537131489346f8c
Martin Dakov <martindakov6@gmail.com>
2023-08-25 23:18:16 +0300

    add file

---
+++ Archive/Lists/Lists - Lab/01. Remove Negatives and Reverse/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Numerics;
+using System.Threading.Tasks;
+namespace _01._Remove_Negatives_and_Reverse
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+            List<int> allNums = numbers.ToList();
+            List<int> nonNegatives = new List<int>();
+            for(int i = 0; i< allNums.Count; i++)
+            {
+                if (allNums[i] > 0)
+                {
+                    nonNegatives.Add(allNums[i]);
+                }
+            }
+            nonNegatives.Reverse();//You can reverse list like that, but you cant reverse array like that, instead use Array.Reverse(array)
+            if(nonNegatives.Count == 0)
+            {
+                Console.WriteLine("empty");
+            }
+            else
+            {
+                Console.WriteLine(string.Join(" ",nonNegatives));
+            }
+        }
+    }
+}
