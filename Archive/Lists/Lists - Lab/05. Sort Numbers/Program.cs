1c3c475fa8a13373f693c8429ad267fc9eaf0c14
Martin Dakov <martindakov6@gmail.com>
2023-08-27 23:06:43 +0300

    add file

---
+++ Archive/Lists/Lists - Lab/05. Sort Numbers/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Numerics;
+using System.Threading.Tasks;
+namespace _05._Sort_Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<double> numbers = Console.ReadLine()
+                .Split()
+                .Select(double.Parse)
+                .ToList();
+             numbers.Sort();
+            Console.WriteLine(string.Join(" <= ",numbers));
+        }
+    }
+}
