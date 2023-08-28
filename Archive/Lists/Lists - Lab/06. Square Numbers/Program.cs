faebb997bfb57df79458894cc8c3c6151f45c521
Martin Dakov <martindakov6@gmail.com>
2023-08-28 22:55:32 +0300

    add file

---
+++ Archive/Lists/Lists - Lab/06. Square Numbers/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Numerics;
+using System.Threading.Tasks;
+namespace _06._Square_Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToList();
+            List<int> squareNums = new List<int>();
+            for(int i = 0; i < numbers.Count; i++)
+            {
+                if (Math.Sqrt(numbers[i]) == (int)(Math.Sqrt(numbers[i])))
+                {
+                    squareNums.Add(numbers[i]);
+                }
+            }
+            squareNums.Sort((x,y) => y.CompareTo(x));
+            Console.WriteLine(string.Join(" ",squareNums));
+        }
+    }
+}
