9395ae1525e2e928e73d25a50a83cb3ed413a65d
Martin Dakov <martindakov6@gmail.com>
2023-08-28 22:55:40 +0300

    add file

---
+++ Archive/Lists/Lists - Lab/07. Count Numbers/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Numerics;
+namespace _07._Count_Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            long[] numbers = Console.ReadLine()
+                .Split()
+                .Select(long.Parse)
+                .ToArray();
+            SortedDictionary<long, int> occurences = new SortedDictionary<long, int>();//Here we keep our nums(for long), and their occurences(for int)
+            for(int i = 0; i < numbers.Length; i++)
+            {
+                if (!occurences.ContainsKey(numbers[i]))                    
+                {
+                    occurences.Add(numbers[i], 1);
+                    for(int j = i + 1; j < numbers.Length; j++)
+                    {
+                        if (numbers[i] == numbers[j])
+                        {
+                            occurences[numbers[i]]++;
+                        }
+                    }
+                }
+            }
+            foreach(var occurence in occurences)
+            {
+                Console.WriteLine($"{occurence.Key} -> {occurence.Value}");
+            }
+        }
+    }
+}
