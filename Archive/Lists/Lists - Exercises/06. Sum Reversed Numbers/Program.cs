5d735748c0970d93721f2c9e431b361ebc113af2
Martin Dakov <martindakov6@gmail.com>
2023-08-31 23:48:28 +0300

    add file

---
+++ Archive/Lists/Lists - Exercises/06. Sum Reversed Numbers/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Numerics;
+using System.Threading.Tasks;
+namespace _06._Sum_Reversed_Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> numbers = Console.ReadLine().Split().ToList();
+            long sum = 0;
+            foreach(var number in numbers)
+            {
+                char[] numToChar = number.ToCharArray();
+                Array.Reverse(numToChar);
+                string num = new string(numToChar);
+                long value = long.Parse(num);
+                sum += value;
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
