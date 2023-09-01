2fbb99727f0f32b21f1403e45919e580e35de5de
Martin Dakov <martindakov6@gmail.com>
2023-09-01 23:28:19 +0300

    add file

---
+++ Archive/Dictionaries/Dictionaries, Lambda and LINQ - Lab/01. Count Real Numbers/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Numerics;
+using System.Threading.Tasks;
+namespace _01._Count_Real_Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            SortedDictionary<double, int> counts = new SortedDictionary<double, int>();
+            double[] numbers = Console.ReadLine().Split().Select(double.Parse).ToArray();
+            for(int i = 0; i < numbers.Length; i++)
+            {
+                if (!counts.ContainsKey(numbers[i]))
+                {
+                    counts.Add(numbers[i], 1);
+                }
+                else
+                {
+                    counts[numbers[i]]++;
+                }                        
+            }
+            foreach(var count in counts)
+            {
+                Console.WriteLine($"{count.Key} -> {count.Value}");
+            }
+        }
+    }
+}
