fa590a4725d2a75b0d7fcce2d94647dd684aa309
Martin Dakov <martindakov6@gmail.com>
2023-09-01 23:28:28 +0300

    add file

---
+++ Archive/Dictionaries/Dictionaries, Lambda and LINQ - Lab/02. Odd Occurrences/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Numerics;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace _02._Odd_Occurrences
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, int> oddWords = new Dictionary<string, int>();
+            string[] words = Console.ReadLine().Split();
+            for(int i = 0; i < words.Length; i++)
+            {
+                if (!oddWords.ContainsKey(words[i].ToLower()))
+                {
+                    oddWords.Add(words[i].ToLower(), 1);
+                }
+                else
+                {
+                    oddWords[words[i].ToLower()]++;
+                }
+            }
+            Console.WriteLine(string.Join(", ", oddWords.Where(x => x.Value % 2 != 0)
+                .Select(x => x.Key)));               
+        }
+    }
+}
