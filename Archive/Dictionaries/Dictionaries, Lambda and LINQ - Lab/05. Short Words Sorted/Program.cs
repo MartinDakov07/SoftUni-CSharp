f0c676ca6bd9cce00ba9d9e7ab96d5e84fa7478e
Martin Dakov <martindakov6@gmail.com>
2023-09-02 23:01:29 +0300

    add file

---
+++ Archive/Dictionaries/Dictionaries, Lambda and LINQ - Lab/05. Short Words Sorted/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Numerics;
+using System.Threading.Tasks;
+namespace _05._Short_Words_Sorted
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine().ToLower();
+            List<string> words = input.Split(new char[] { '.', ',', ':', ';', '(', ')', '[', ']', '\"', '\'', '/', '\\', '!', '?', ' '},StringSplitOptions.RemoveEmptyEntries).ToList();
+            Console.WriteLine(string.Join(", ",words.Where(x=>x.Length < 5).OrderBy(x=>x).Distinct()));
+
+        }
+    }
+}
