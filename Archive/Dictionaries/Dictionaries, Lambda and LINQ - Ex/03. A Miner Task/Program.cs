1e6a062f41ddd6e610ede7910321b36857222291
Martin Dakov <martindakov6@gmail.com>
2023-09-05 23:37:50 +0300

    add file

---
+++ Archive/Dictionaries/Dictionaries, Lambda and LINQ - Ex/03. A Miner Task/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Numerics;
+using System.Threading.Tasks;
+using System.Text;
+namespace _03._A_Miner_Task
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, int> resources = new Dictionary<string, int>();
+            string resource = Console.ReadLine();
+            while(resource != "stop")
+            {
+                int quantity = int.Parse(Console.ReadLine());
+                if (!resources.ContainsKey(resource))
+                {
+                    resources.Add(resource, quantity);
+                }
+                else
+                {
+                    resources[resource] += quantity;
+                }
+                resource = Console.ReadLine();
+            }
+             foreach(var treasure in resources)
+            {
+                Console.WriteLine($"{treasure.Key} -> {treasure.Value}");
+            }
+        }
+    }
+}
