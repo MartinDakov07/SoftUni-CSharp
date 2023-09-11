3d440e67d3a561fb9f5100f919b4a2e9280eea00
Martin Dakov <martindakov6@gmail.com>
2023-09-11 22:56:18 +0300

    add file

---
+++ Archive/Dictionaries/Dictionaries, Lambda and LINQ - ME/01. Sort Times/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+using System.Numerics;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Text;
+namespace _01._Sort_Times
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var times = Console.ReadLine().Split().OrderBy(t=>t).ToList();          
+            Console.WriteLine(string.Join(", ",times));
+        }
+    }
+}
