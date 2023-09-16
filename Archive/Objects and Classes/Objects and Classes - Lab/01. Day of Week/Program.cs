83f06f1cb51459dcc218e228e2075820b411622f
Martin Dakov <martindakov6@gmail.com>
2023-09-16 20:27:49 +0300

    add file

---
+++ Archive/Objects and Classes/Objects and Classes - Lab/01. Day of Week/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Numerics;
+using System.Threading.Tasks;
+using System.Text;
+using System.Globalization;
+
+namespace _01._Day_of_Week
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string date = Console.ReadLine();
+            DateTime week = DateTime.ParseExact(date, "d-M-yyyy", CultureInfo.InvariantCulture);
+            //int[] dateArg = date.Split('-').Select(int.Parse).ToArray();
+            //new DateTime(year: dateArg[2], month: dateArg[1], day: dateArg[0]);
+            Console.WriteLine(week.DayOfWeek);
+        }
+    }
+}
