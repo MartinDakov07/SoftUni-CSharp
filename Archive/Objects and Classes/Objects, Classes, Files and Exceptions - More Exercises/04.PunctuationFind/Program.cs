4718acb1902957af80ffe525ac6bb85154937c30
Martin Dakov <martindakov6@gmail.com>
2023-09-26 22:52:01 +0300

    add file

---
+++ Archive/Objects and Classes/Objects, Classes, Files and Exceptions - More Exercises/04.PunctuationFind/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+using System.Numerics;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Text;
+using System.IO;
+namespace _04._Punctuation_Finder
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            String[] lines = File.ReadAllLines(@"C:\Users\User\OneDrive\Documents\sample_text.txt");
+            List<char> separators = new List<char>();
+            char[] seps = new char[] { ',', '.', '?', '!', ':' };
+            foreach(string line in lines)
+            {
+                for(int i = 0; i < line.Length; i++)
+                {
+                    if (seps.Contains(line[i]))
+                    {
+                        separators.Add(line[i]);
+                    }
+                }
+            }
+            Console.WriteLine(string.Join(", ",separators));
+        }
+    }
+}
