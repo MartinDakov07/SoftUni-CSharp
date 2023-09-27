63583249bee5e77c6c576db98a164fbbdbfc66d5
Martin Dakov <martindakov6@gmail.com>
2023-09-27 22:37:28 +0300

    add file

---
+++ Archive/Objects and Classes/Objects, Classes, Files and Exceptions - More Exercises/05. Write to File/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+using System.IO;
+using System.Numerics;
+using System.Text;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace _05._Write_to_File
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var lines = File.ReadAllText(@"C:\Users\User\OneDrive\Documents\sample_text.txt");
+            var chars = lines.Where(c => ".,?!:".IndexOf(c) == -1).ToArray();
+            File.WriteAllText(@"C:\Users\User\OneDrive\Documents\output.txt", string.Join("", chars));
+            //you should paste the content of the file in judge not the solution
+        }
+    }
+}
