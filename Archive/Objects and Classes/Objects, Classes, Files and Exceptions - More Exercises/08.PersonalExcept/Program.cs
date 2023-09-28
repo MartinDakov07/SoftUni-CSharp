4f5ed99a0e5b2f1cdd0c548e2443c159d8050e72
Martin Dakov <martindakov6@gmail.com>
2023-09-28 21:45:12 +0300

    add file

---
+++ Archive/Objects and Classes/Objects, Classes, Files and Exceptions - More Exercises/08.PersonalExcept/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Linq;
+using System.Numerics;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Text;
+namespace _08._Personal_Exception
+{
+    class PersonalException : Exception
+    {
+        public PersonalException() : base("My first exception is awesome!!!")
+        {
+            
+        }       
+    }
+    internal class Program
+    {
+        
+        static void Main(string[] args)
+        {            
+            try
+            {
+                while (true)
+                {
+                    int number = int.Parse(Console.ReadLine());
+                    if(number < 0)
+                    {
+                        throw new PersonalException();
+                    }
+                    Console.WriteLine(number);
+                }
+            }
+            catch(PersonalException errorMessage)
+            {
+                Console.WriteLine(errorMessage.Message);
+                return;
+            }
+        }
+    }
+}
