1672781ac5f450f6114d08b872c23987d002fee0
Martin Dakov <martindakov6@gmail.com>
2023-09-29 22:52:33 +0300

    add file

---
+++ Archive/Strings and Text Processing/Strings and Text Processing - Lab/01. Reverse String/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+
+namespace _01._Reverse_String
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string message = Console.ReadLine();
+            char[] messageToChar = message.ToCharArray();
+            Array.Reverse(messageToChar);
+            string reversed = new string(messageToChar);
+            Console.WriteLine(reversed);
+        }
+    }
+}
