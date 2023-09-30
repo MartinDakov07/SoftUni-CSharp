72d59d38b472ebb5e56cb31ad57ea8c66fefa1f2
Martin Dakov <martindakov6@gmail.com>
2023-09-30 22:55:31 +0300

    add file

---
+++ Archive/Strings and Text Processing/Strings and Text Processing - Lab/02.CountSubstringOcc/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+namespace _02._Count_Substring_Occurrences
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine().ToLower();
+            string textToFind = Console.ReadLine().ToLower();
+            int findTextLenght = textToFind.Length;
+            int matchCount = 0;
+            for (int i = 0; i < text.Length; i++) 
+            {
+                if(i + findTextLenght > text.Length)
+                {
+                    break;
+                }
+                string currWord = text.Substring(i, findTextLenght);
+                if(currWord == textToFind)
+                {
+                    matchCount++;
+                }
+            }
+            Console.WriteLine(matchCount);
+        }
+    }
+}
