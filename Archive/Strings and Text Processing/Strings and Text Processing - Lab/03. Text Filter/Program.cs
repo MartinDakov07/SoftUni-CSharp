4aeebedaedfa79aab67a92f32ff3011586fad3a3
Martin Dakov <martindakov6@gmail.com>
2023-10-01 23:08:32 +0300

    add file

---
+++ Archive/Strings and Text Processing/Strings and Text Processing - Lab/03. Text Filter/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Numerics;
+using System.Threading.Tasks;
+using System.Text;
+namespace _03._Text_Filter
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] banWords = Console.ReadLine().Split(", ", StringSplitOptions.RemoveEmptyEntries);
+            string text = Console.ReadLine();
+            foreach(var word in banWords)
+            {
+                text = text.Replace(word, new string('*', word.Length));
+            }
+            Console.WriteLine(text);
+        }
+    }
+}
