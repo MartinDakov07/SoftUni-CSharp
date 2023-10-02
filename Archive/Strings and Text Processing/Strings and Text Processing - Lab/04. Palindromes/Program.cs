d99d11b8658eecc867b0c14e1fafa081e1c91069
Martin Dakov <martindakov6@gmail.com>
2023-10-02 23:00:45 +0300

    add file

---
+++ Archive/Strings and Text Processing/Strings and Text Processing - Lab/04. Palindromes/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+using System.Numerics;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Text;
+namespace _04._Palindromes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] text = Console.ReadLine().Split(new[] { ' ', '.', ',', '?', '!' }, StringSplitOptions.RemoveEmptyEntries);
+            List<string> palindromes = new List<string>();
+            foreach(string word in text)
+            {
+
+                string reversedWord = new string(word.Reverse().ToArray());
+                if(word == reversedWord)
+                {
+                    if (!palindromes.Contains(word))
+                    {
+                        palindromes.Add(word);
+                    }
+                }                
+            }
+            Console.WriteLine(string.Join(", ",palindromes.OrderBy(p=>p)));
+        }
+    }
+}
