8cccc0af4ca541fae7b520cbd88fbcefe6e05f44
Martin Dakov <martindakov6@gmail.com>
2023-10-03 22:41:15 +0300

    add file

---
+++ Archive/Strings and Text Processing/StringsAndTextProcessing-Ex/01.ConvFromBase10-N/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Numerics;
+using System.Text;
+using System.Globalization;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace _01._Convert_from_base_10_to_base_N
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {            
+            List<BigInteger> numbers = Console.ReadLine().Split().Select(BigInteger.Parse).ToList();
+            int n = (int)numbers[0];
+            BigInteger num = numbers[1];
+            BigInteger remainder;
+            string result = null;
+            if(n>=2 && n <= 10)
+            {
+                while(num > 0)
+                {
+                    remainder = num % n;
+                    num /= n;
+                    result = remainder.ToString() + result;
+                }
+                Console.WriteLine(result);
+            }
+            else
+            {
+                Console.WriteLine(0);
+            }
+        }
+    }
+}
