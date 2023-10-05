fc7034984dc7f231d5273c55911425a8db713488
Martin Dakov <martindakov6@gmail.com>
2023-10-05 23:25:08 +0300

    add file

---
+++ Archive/Strings and Text Processing/StringsAndTextProcessing-Ex/03. Unicode Characters/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+using System.Numerics;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+namespace _03._Unicode_Characters
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine();
+            foreach(var letter in text)
+            {
+                Console.Write("\\u" + ((int)letter).ToString("x4"));
+            }
+        }
+    }
+}
