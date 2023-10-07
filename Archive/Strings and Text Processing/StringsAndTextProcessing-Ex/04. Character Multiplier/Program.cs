652194142601532e7721fb8b6d78d5f5def0f965
Martin Dakov <martindakov6@gmail.com>
2023-10-07 19:44:46 +0300

    add file

---
+++ Archive/Strings and Text Processing/StringsAndTextProcessing-Ex/04. Character Multiplier/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Linq;
+using System.Numerics;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Text;
+namespace _04._Character_Multiplier
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split();
+            string firstWord = input[0];
+            string secondWord = input[1];
+            long result = 0;
+            if (firstWord.Length == secondWord.Length)
+            {                
+                for(int i = 0; i < firstWord.Length; i++)
+                {
+                    int firstValue = (int)firstWord[i];
+                    int secondValue = (int)secondWord[i];
+                    result += firstValue * secondValue;
+                }
+            }
+            else
+            {
+                int lowerLength = Math.Min(firstWord.Length, secondWord.Length);
+                
+                if(firstWord.Length > secondWord.Length)
+                {                   
+                    for(int i = 0; i < lowerLength; i++)
+                    {
+                        int firstValue = (int)firstWord[i];
+                        int secondValue = (int)secondWord[i];
+                        result += firstValue * secondValue;
+                    }
+                    for(int i = lowerLength; i < firstWord.Length; i++)
+                    {
+                        result += (int)firstWord[i];
+                    }
+                }
+                else
+                {
+                    for (int i = 0; i < lowerLength; i++)
+                    {
+                        int firstValue = (int)firstWord[i];
+                        int secondValue = (int)secondWord[i];
+                        result += firstValue * secondValue;
+                    }
+                    for (int i = lowerLength; i < secondWord.Length; i++)
+                    {
+                        result += (int)secondWord[i];
+                    }
+                }
+            }
+            Console.WriteLine(result);
+        }
+    }
+}
