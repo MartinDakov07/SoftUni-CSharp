23916a2201be468c4c30f67d29cc05f1f87d63b1
Martin Dakov <martindakov6@gmail.com>
2023-10-08 22:35:48 +0300

    add file

---
+++ Archive/Strings and Text Processing/StringsAndTextProcessing-Ex/05. Magic exchangeable words/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Numerics;
+using System.Threading.Tasks;
+using System.Text;
+namespace _05._Magic_exchangeable_words
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] words = Console.ReadLine().Split();
+            string firstWord = words[0];
+            string secondWord = words[1];
+            bool isExchangable = true;
+            Dictionary<char, char> corresponds = new Dictionary<char, char>();
+
+            int lowerLength = Math.Min(firstWord.Length, secondWord.Length);
+            for (int i = 0; i < lowerLength; i++)
+            {
+                if (!corresponds.ContainsKey(firstWord[i]))
+                {
+                    if (corresponds.ContainsValue(secondWord[i]))
+                    {
+                        isExchangable = false;
+                    }
+                    else
+                    {
+                        corresponds.Add(firstWord[i], secondWord[i]);
+                    }
+                }
+                else
+                {                  
+                    if (corresponds[firstWord[i]] != secondWord[i])
+                    {
+                        isExchangable = false;
+                    }
+                }
+            }                                      
+            if(firstWord.Length > secondWord.Length)
+            {
+                
+                for(int i = lowerLength; i < firstWord.Length; i++)
+                {
+                    if (!corresponds.ContainsKey(firstWord[i]))
+                    {
+                        isExchangable = false;
+                    }
+                }
+            }
+            else if(firstWord.Length < secondWord.Length)
+            {                   
+                for (int i = lowerLength; i < secondWord.Length; i++)
+                {
+                    if (!corresponds.ContainsValue(secondWord[i]))
+                    {
+                        isExchangable = false;
+                    }
+                }
+            }
+            
+            if (isExchangable)
+            {
+                Console.WriteLine("true");
+            }
+            else
+            {
+                Console.WriteLine("false");
+            }
+        }
+    }
+}
