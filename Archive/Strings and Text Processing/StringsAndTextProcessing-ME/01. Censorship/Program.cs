e95b4cb33e57213a280fed3ac99eba1622a5bbfe
Martin Dakov <martindakov6@gmail.com>
2023-10-13 20:04:12 +0300

    add file

---
+++ Archive/Strings and Text Processing/StringsAndTextProcessing-ME/01. Censorship/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+using System.Numerics;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+namespace _01._Censorship
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string word = Console.ReadLine();
+            string sentence = Console.ReadLine();
+            sentence = sentence.Replace(word, new string('*', word.Length));
+            Console.WriteLine(sentence);
+        }
+    }
+}
