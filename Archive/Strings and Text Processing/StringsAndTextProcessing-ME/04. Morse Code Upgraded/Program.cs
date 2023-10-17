d5ca4cf1fc53757cb1f160fe2c37346672f82b62
Martin Dakov <martindakov6@gmail.com>
2023-10-17 23:09:34 +0300

    add file

---
+++ Archive/Strings and Text Processing/StringsAndTextProcessing-ME/04. Morse Code Upgraded/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Linq;
+using System.Numerics;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+using System.Text;
+namespace _04._Morse_Code_Upgraded
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] text = Console.ReadLine().Split("|",StringSplitOptions.RemoveEmptyEntries);
+            StringBuilder message = new StringBuilder();
+            foreach(var letter in text)
+            {
+                int one = 0;
+                int zero = 0;
+                int currSum = 0;
+                for(int i = 0; i < letter.Length; i++)
+                {
+                    if (letter[i] == '1')
+                    {
+                        one++;
+                    }
+                    else if (letter[i] == '0')
+                    {
+                        zero++;
+                    }
+                }
+                currSum = one * 5 + zero * 3;
+                int count = 1;
+                for(int i = letter.Length - 1; i > 0; i--)
+                {
+                    if (letter[i] == letter[i - 1])
+                    {
+                        count++;
+                    }
+                    else
+                    {
+                        if(count != 1)
+                        {
+                            currSum += count;
+                            count = 1;
+                        }                        
+                    }
+                }
+                if (count != 1)
+                {
+                    currSum += count;                    
+                }
+                message.Append((char)currSum);
+            }
+            Console.WriteLine(message);
+        }
+    }
+}
