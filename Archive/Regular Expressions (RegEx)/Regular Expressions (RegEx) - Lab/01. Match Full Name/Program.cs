cad6002812e51edb9f1c4ece23954d143e97fb4d
Martin Dakov <martindakov6@gmail.com>
2023-10-24 23:06:06 +0300

    add file

---
+++ Archive/Regular Expressions (RegEx)/Regular Expressions (RegEx) - Lab/01. Match Full Name/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+using System.Numerics;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+using System.Diagnostics.Tracing;
+
+namespace _01._Match_Full_Name
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string regex = @"\b([A-Z][a-z]+ [A-Z][a-z]+\b)";
+            string input = Console.ReadLine();
+            MatchCollection matches = Regex.Matches(input, regex);
+            foreach(Match match in matches)
+            {
+                Console.Write($"{match.Value} ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
