3cef699079a6b6522364a30ad278940e5d46e778
Martin Dakov <martindakov6@gmail.com>
2023-10-26 22:48:36 +0300

    add file

---
+++ Archive/Regular Expressions (RegEx)/Regular Expressions (RegEx) - Lab/03.MatchHexalNumbers/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+using System.Numerics;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+namespace _03._Match_Hexadecimal_Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string regex = @"\b(?:0x)?[0-9A-F]+\b";
+            string input = Console.ReadLine();
+            var matches = Regex.Matches(input, regex).Cast<Match>().Select(v => v.Value).ToArray();
+            Console.WriteLine(string.Join(" ",matches));
+        }
+    }
+}
