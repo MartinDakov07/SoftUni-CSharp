93e9e59eb6f90be0769d0613f7d6f8d45c37af9a
Martin Dakov <martindakov6@gmail.com>
2023-10-30 09:56:36 +0200

    add file

---
+++ Archive/Regular Expressions (RegEx)/Regular Expressions (RegEx) - Lab/04. Match Dates/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+using System.Numerics;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+namespace _04._Match_Dates
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string regex = @"\b(?<day>\d{2})([\/\.\-])(?<month>[A-Z][a-z]{2})\1(?<year>\d{4})\b";
+            var input = Console.ReadLine();
+            var matches = Regex.Matches(input, regex);
+            foreach(Match match in matches)
+            {
+                var day = match.Groups["day"].Value;
+                var month = match.Groups["month"].Value;
+                var year = match.Groups["year"].Value;
+
+                Console.WriteLine($"Day: {day}, Month: {month}, Year: {year}");
+            }
+        }
+    }
+}
