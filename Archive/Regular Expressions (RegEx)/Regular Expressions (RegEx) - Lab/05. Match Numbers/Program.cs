fbd500bce7b020a1639dbba897fd477477c878a9
Martin Dakov <martindakov6@gmail.com>
2023-10-31 17:10:00 +0200

    add file

---
+++ Archive/Regular Expressions (RegEx)/Regular Expressions (RegEx) - Lab/05. Match Numbers/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+using System.Numerics;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+namespace _05._Match_Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string pattern = @"(^|(?<=\s))-?\d+(\.\d+)?($|(?=\s))";
+            string input = Console.ReadLine();
+            MatchCollection matches = Regex.Matches(input, pattern);
+            foreach(Match match in matches)
+            {
+                Console.Write($"{match.Value} ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
