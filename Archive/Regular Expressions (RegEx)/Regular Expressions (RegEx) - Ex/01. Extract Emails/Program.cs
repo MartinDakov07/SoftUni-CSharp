d4a6c379fc63dea7c6ff15c423a51f5b1fcaed90
Martin Dakov <martindakov6@gmail.com>
2023-11-02 23:21:56 +0200

    add file

---
+++ Archive/Regular Expressions (RegEx)/Regular Expressions (RegEx) - Ex/01. Extract Emails/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+using System.Numerics;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+namespace _01._Extract_Emails
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string pattern = @"(^|(?<=\s))(([a-zA-Z0-9]+)([\.\-_]?)([A-Za-z0-9]+)(@)([a-zA-Z]+([\.\-_][A-Za-z]+)+))(\b|(?=\s))";
+            string input = Console.ReadLine();
+            List<Match> emails = new List<Match>();
+            MatchCollection matches = Regex.Matches(input, pattern);
+            foreach(Match match in matches)
+            {
+                emails.Add(match);
+            }
+            Console.WriteLine(string.Join(Environment.NewLine,emails.Select(e=>e.Value)));
+        }
+    }
+}
