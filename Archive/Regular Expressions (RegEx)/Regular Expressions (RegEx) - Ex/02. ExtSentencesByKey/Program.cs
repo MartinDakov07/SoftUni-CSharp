5a6171b7fb03696cf3855e4ba5b7a67c59ac8287
Martin Dakov <martindakov6@gmail.com>
2023-11-05 22:38:42 +0200

    add file

---
+++ Archive/Regular Expressions (RegEx)/Regular Expressions (RegEx) - Ex/02. ExtSentencesByKey/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+using System.Numerics;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+namespace _02._Extract_Sentences_by_Keyword
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string word = Console.ReadLine();
+            Regex pattern = new Regex($@"\b[^?.!]*\b{word}\b[^?.!]*");
+
+            string text = Console.ReadLine();
+
+            MatchCollection matches = pattern.Matches(text);
+
+            foreach (Match match in matches)
+            {
+                string sentence = match.Value.Trim();
+                Console.WriteLine(sentence);
+            }
+
+        }
+    }
+}
