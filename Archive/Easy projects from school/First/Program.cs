74825147f73001992c9a6ef2f5d573aeaf9cbdad
Martin Dakov <martindakov6@gmail.com>
2024-02-17 22:58:17 +0200

    add

---
+++ Archive/Easy projects from school/First/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Runtime.Serialization.Formatters;
+
+namespace demo2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //ako e string
+            string text = Console.ReadLine();
+            int middle = text.Length / 2;
+            if(text.Length <= 20)
+            {
+                text = text.Remove(middle, text.Length - middle);
+                for(int i = middle; i <= 20; i++)
+                {
+                    text = text.Insert(i, "*");
+                }
+            }
+            Console.WriteLine(text);
+        }
+    }
+}
