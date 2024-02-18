46a90449f4964ddbc884719964db6893758a1f3f
Martin Dakov <martindakov6@gmail.com>
2024-02-18 23:38:02 +0200

    add

---
+++ Archive/Easy projects from school/Second/leetcode/Program.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using System;
+using System.Numerics;
+namespace leetcode
+{
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string ransomNote = Console.ReadLine();
+            string magazine = Console.ReadLine();
+            bool canConstruct = CanConstruct(ransomNote, magazine);
+        }
+        static public bool CanConstruct(string ransomNote, string magazine)
+        {
+            List<char> letters = new List<char>();
+            for (int i = 0; i < magazine.Length; i++)
+            {
+                letters.Add(magazine[i]);
+            }
+            for (int i = 0; i < ransomNote.Length; i++)
+            {
+                if (letters.Contains(ransomNote[i]))
+                {
+                    letters.Remove(ransomNote[i]);
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+    }
+}
