2735d0486d3ecfb3b03e96f4789ce5fc2c60d9a4
Martin Dakov <martindakov6@gmail.com>
2024-02-19 22:56:48 +0200

    add

---
+++ Archive/Easy projects from school/Third/Minor problem/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Linq;
+using System.Numerics;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+namespace Minor_problem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, int> loot = new Dictionary<string, int>();
+
+            string resource = Console.ReadLine();
+            while(resource != "stop")
+            {
+                int quantity = int.Parse(Console.ReadLine());
+                if (!loot.ContainsKey(resource))
+                {
+                    loot.Add(resource, 0);
+                }
+                loot[resource] += quantity;
+                resource = Console.ReadLine();
+            }
+           foreach(var resources in loot)
+            {
+                Console.WriteLine($"{resources.Key} -> {resources.Value}");
+            }
+        }
+    }
+}
