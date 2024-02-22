75b0e7d2dfa41f05bca5b6bf147bebc1e23dfae1
Martin Dakov <martindakov6@gmail.com>
2024-02-22 22:56:19 +0200

    add

---
+++ Archive/Easy projects from school/Fourth/demo solution/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Numerics;
+namespace _03._MOBA_Challenger
+{
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string ransomNote = Console.ReadLine();
+            string magazine = Console.ReadLine();
+            bool canConstruct = CanConstruct(ransomNote, magazine);
+        }
+        static public bool CanConstruct(string ransomNote, string magazine)
+        {
+            List<char> letters = new List<char>();
+            for (int i = 0; i < magazine.Length; i++)
+            {
+                letters.Add(magazine[i]);
+            }
+            for (int i = 0; i < ransomNote.Length; i++)
+            {
+                if (!letters.Contains(ransomNote[i]))
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+    }
+}
